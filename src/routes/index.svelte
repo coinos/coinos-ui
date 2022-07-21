91b7c821991bbf69b7a6990432b59dafbfc970db
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-07-21 12:20:17 -0600

    responsive updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -50,9 +50,9 @@
 </script>
 
 <header class="w-full lg:w-5/6 pt-10 mx-auto">
-	<nav class="block lg:flex flex-wrap justify-between items-center">
+	<nav class="block xl:flex flex-wrap justify-between items-center">
 		<a href="/">
-			<img src="/icons/logo.svg" alt="logo" class="mx-auto mb-5 lg:mb-0" />
+			<img src="/icons/logo.svg" alt="logo" class="mx-auto mb-5 xl:mb-0" />
 		</a>
 		<div class="hidden space-x-10 md:flex flex-wrap justify-center items-center font-bold">
 			<button on:click={() => scroll(howItWorks)}>How It Works</button>
