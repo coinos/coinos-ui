7ad85953f44f25b8c341bff94fd939014a7dd52e
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-07-21 12:48:46 -0600

    move landing header into comp

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as LandingHeader } from './LandingHeader.svelte';
 export { default as LandingInfoCard } from './LandingInfoCard.svelte';
 export { default as HowItWorksCard } from './HowItWorksCard.svelte';
 export { default as FaqCard } from './FaqCard.svelte';
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,10 @@
 <script>
-	import { scroll } from '../lib/utils';
 	import {
+		LandingHeader,
 		LandingInfoCard,
 		HowItWorksCard,
 		FaqCard,
-		PrimaryButton,
-		SecondaryButton
+		PrimaryButton
 	} from '../components';
 
 	let howItWorks;
@@ -49,20 +48,7 @@
 	];
 </script>
 
-<header class="w-full lg:w-5/6 pt-10 mx-auto">
-	<nav class="block xl:flex flex-wrap justify-between items-center">
-		<a href="/">
-			<img src="/icons/logo.svg" alt="logo" class="mx-auto mb-5 xl:mb-0" />
-		</a>
-		<div class="hidden space-x-10 md:flex flex-wrap justify-center items-center font-bold">
-			<button on:click={() => scroll(howItWorks)}>How It Works</button>
-			<button on:click={() => scroll(faq)}>FAQ</button>
-			<button>About</button>
-			<SecondaryButton title="Start in Seconds" />
-			<PrimaryButton title="Login" />
-		</div>
-	</nav>
-</header>
+<LandingHeader {howItWorks} {faq} />
 
 <main class="space-y-40 py-20 md:py-32 lg:py-40 px-5 md:px-0">
 	<!-- hero -->
