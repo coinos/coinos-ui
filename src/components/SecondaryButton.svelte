4ae7855829faacfc5c29ff3284ef0d751bb2bc6c
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-07-21 11:04:30 -0600

    create more components to DRY up code

---
+++ src/components/SecondaryButton.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let title;
+	export let click;
+</script>
+
+<button class="border rounded-full px-6 py-2 font-bold" on:click={click}>{title}</button>
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
+export { default as LandingInfoCard } from './LandingInfoCard.svelte';
 export { default as HowItWorksCard } from './HowItWorksCard.svelte';
 export { default as FaqCard } from './FaqCard.svelte';
+export { default as PrimaryButton } from './PrimaryButton.svelte';
+export { default as SecondaryButton } from './SecondaryButton.svelte';
