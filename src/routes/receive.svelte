b6907bfae1a96f1d1ef99c7575598ca591c0d6d7
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-07-22 14:34:18 -0600

    update chevron icon so it uses currentColor

--- src/routes/receive.svelte
+++ src/routes/receive.svelte
@@ -5,7 +5,6 @@
 	let useFiat = true;
 	let amountFiat = 0;
 	let amountSats = 0;
-	let deleteActive = false;
 
 	const numPad = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0', '<'];
 </script>
@@ -33,12 +32,23 @@
 				{#if value === '<'}
 					<button
 						class="bg-primary rounded-xl py-4 px-8 font-semibold active:bg-black active:text-white"
-						on:mousedown={() => (deleteActive = true)}
-						on:mouseup={() => (deleteActive = false)}
-						><Icon
-							icon={deleteActive ? 'chevron-left-white' : 'chevron-left'}
-							style="w-5"
-						/></button
+					>
+						<svg
+							class="w-5"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M15 19L8 12L15 5"
+								stroke="currentColor"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg></button
 					>
 				{:else}
 					<button
