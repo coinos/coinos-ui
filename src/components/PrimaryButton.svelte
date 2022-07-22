69ae5e2b84a7eedc71c7219af2e25d6de815b801
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-07-22 13:24:51 -0600

    add numpad/generate invoice step 1 to receive page, refactoring

--- src/components/PrimaryButton.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	export let title;
-	export let click;
-</script>
-
-<button class="bg-black text-white border rounded-full px-6 py-2 font-bold" on:click={click}>
-	{title}
-</button>
--- src/components/SecondaryButton.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	export let title;
-	export let click;
-</script>
-
-<button class="border rounded-full px-6 py-2 font-bold" on:click={click}>{title}</button>
