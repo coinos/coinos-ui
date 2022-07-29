4659bd444ac11a2dff6f65412de6c7a28e086a66
Adrien Hopkins <ahopk127@my.yorku.ca>
2022-07-29 15:54:30 -0500

    made the locale selector its own component

---
+++ src/components/LocaleSelector.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { _, dictionary, locale, locales } from 'svelte-i18n';
+</script>
+
+<select bind:value={$locale}>
+  {#each $locales as locale}
+      <option value={locale}>{locale}</option>
+  {/each}
+</select>
