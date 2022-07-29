b9fff25b8ecb0aad18aa7d7f7b853cc5527f1c00
Adrien Hopkins <ahopk127@my.yorku.ca>
2022-07-29 13:56:46 -0500

    If regional locale not found, uses language locale instead
    (e.g. uses en if en-US is not available)

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -9,7 +9,32 @@ function addLocale(localeCode) {
 }
 localeCodes.forEach(addLocale);
 
+// determine locale
+function pickLocale(navigatorLocale, availableLocales) {
+  // determine langugage part of locale
+  // (e.g. if navigatorLocale is 'en-US', languageCode is 'en')
+  let dashIndex = navigatorLocale ? navigatorLocale.indexOf("-") : -1;
+  let languageCode; // navigator locale without the regional part
+  if (dashIndex === -1) {
+    languageCode = navigatorLocale;
+  } else {
+    languageCode = navigatorLocale.slice(0, dashIndex);
+  }
+
+  // get locale
+  if (availableLocales.includes(navigatorLocale)) {
+    return navigatorLocale;
+  } else if (availableLocales.includes(languageCode)) {
+    return languageCode;
+  } else {
+    return null;
+  }
+}
+
+let initialLocale = pickLocale(getLocaleFromNavigator(), localeCodes);
+console.log("Selected locale '" + initialLocale + "'");
+
 init({
   fallbackLocale: 'en',
-  initialLocale: getLocaleFromNavigator(),
+  initialLocale: initialLocale,
 });
