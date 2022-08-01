b1913e4a7818d293685ab49c146d5e4f7e9543e5
Adam Soltys <asoltys@gmail.com>
2022-08-01 13:27:45 -0700

    check username exists

--- src/routes/[username]/index.js
+++ src/routes/[username]/index.js
@@ -1,3 +1,14 @@
+import { get } from '$lib/utils';
+
 export async function GET({ params }) {
-	return { body: params };
+	let { username } = params;
+	let body;
+
+  try {
+    body = await get(`/users/${username}`);
+  } catch(e) {
+    body = {};
+  } 
+
+	return { body };
 }
