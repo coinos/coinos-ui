2b232a584144a92188b1ef337e90c7848b007dbe
Adam Soltys <asoltys@gmail.com>
2022-08-01 11:32:46 -0700

    pass amount to lightning invoice endpoint

--- src/routes/invoice/index.js
+++ src/routes/invoice/index.js
@@ -4,7 +4,7 @@ let network = 'lightning';
 
 export async function POST({ request }) {
 	let { amount, rate, username } = await request.json();
-	let { text } = await post('/lightning/invoice');
+	let { text } = await post('/lightning/invoice', { amount });
 	let { uuid: id } = await post('/invoice', {
 		invoice: { amount, currency: 'USD', network, rate, text },
 		user: { username }
