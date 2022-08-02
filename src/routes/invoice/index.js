05e05e6282ebec650e87bd1277a1f8a32e5be742
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-08-01 22:04:19 -0600

    format

--- src/routes/invoice/index.js
+++ src/routes/invoice/index.js
@@ -4,7 +4,7 @@ let network = 'lightning';
 
 export async function POST({ request }) {
 	let { amount, rate, username, text } = await request.json();
-  if (!text) ({ text } = await post('/lightning/invoice', { amount }));
+	if (!text) ({ text } = await post('/lightning/invoice', { amount }));
 	let { uuid: id } = await post('/invoice', {
 		invoice: { amount, currency: 'USD', network, rate, text },
 		user: { username }
