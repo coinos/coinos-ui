41297da90b95a677af32398c11c29e3f7fae09d7
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-08-02 21:27:18 -0600

    update new payment notification

--- src/components/AppHeader.svelte
+++ src/components/AppHeader.svelte
@@ -40,11 +40,12 @@
 				<button class="bg-white p-2 rounded-full w-12 h-12 relative"
 					><Icon icon="clock" style="mx-auto" />
 					{#if $newPayment}
-						<span class="absolute top-[8px] right-[8px]">
-							<span class="flex h-8 w-8">
+						<span class="absolute top-0 right-0">
+							<span class="flex h-3 w-3">
 								<span
 									class="animate-ping absolute inline-flex h-full w-full rounded-full bg-[#F7931A] opacity-75"
 								/>
+								<span class="relative inline-flex rounded-full h-3 w-3 bg-[#F7931A]" />
 							</span>
 						</span>
 					{/if}
