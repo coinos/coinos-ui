499ff7649b76857858bb1eefd6ece87e391e7a63
Adrien Hopkins <ahopk127@my.yorku.ca>
2022-08-04 12:32:13 -0500

    Possible fix for the page breaking the first time you open it

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -6,6 +6,9 @@ const localeCodes = ['en', 'fr'];
 // The directory where locale files are stored
 const localeDirectory = "../locales";
 
+// the default locale to use if none was found
+const defaultLocale = "en";
+
 // Adds a new locale based on its code
 function addLocale(localeCode) {
   register(localeCode, () => import(`${localeDirectory}/${localeCode}.json`));
@@ -13,6 +16,11 @@ function addLocale(localeCode) {
 
 // Picks the default locale based on navigator locale and the list of available locales
 function pickLocale(navigatorLocale, availableLocales) {
+  // locale can be null the first time you load this
+  // return the default locale in that case - reloading should fix this
+  if (!navigatorLocale)
+    return defaultLocale;
+
   // determine langugage part of locale
   // (e.g. if navigatorLocale is 'en-US', languageCode is 'en')
   let dashIndex = navigatorLocale ? navigatorLocale.indexOf("-") : -1;
@@ -38,6 +46,6 @@ localeCodes.forEach(addLocale);
 
 let initialLocale = pickLocale(getLocaleFromNavigator(), localeCodes);
 init({
-  fallbackLocale: 'en',
+  fallbackLocale: defaultLocale,
   initialLocale: initialLocale,
 });
