657566b7c6a3e412d7691a0acab53a3689c99f33
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-08-16 10:57:02 -0600

    add style prop to locale selector comp

--- src/components/LocaleSelector.svelte
+++ src/components/LocaleSelector.svelte
@@ -1,8 +1,9 @@
 <script>
   import { _, locale, locales } from 'svelte-i18n';
+  export let style
 </script>
 
-<select bind:value={$locale}>
+<select name='language' bind:value={$locale} class={style}>
   {#each $locales as locale}
     <option value={locale}>{locale}</option>
   {/each}
