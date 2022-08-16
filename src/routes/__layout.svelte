43159f41602cb1502fabe240d30c5d5f87ce3581
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-08-16 10:57:55 -0600

    wait on locale load to render app

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,12 @@
+<script context="module">
+  import { waitLocale } from 'svelte-i18n'
+
+  export async function preload() {
+    // awaits for the loading of the 'en-US' and 'en' dictionaries
+    return waitLocale()
+  }
+</script>
+
 <script>
 	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import '../app.css';
