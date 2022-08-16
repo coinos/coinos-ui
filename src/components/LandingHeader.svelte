148759ba44fb01d33dc770e4f074cb2ae37af46e
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-08-16 10:58:34 -0600

    update styling and placment on selector in nav and update json strings

--- src/components/LandingHeader.svelte
+++ src/components/LandingHeader.svelte
@@ -18,27 +18,29 @@
 
 <header class="w-full lg:w-5/6 py-5 mx-auto fixed md:sticky z-10 top-0 bg-white/90">
 	<nav class="block xl:flex flex-wrap justify-between items-center">
+<div class='flex justify-start md:justify-center items-center md:space-x-10'>
 		<a href="/" class="hidden md:block">
-			<Icon icon="logo" style="mx-auto mb-5 xl:mb-0" />
+			<Icon icon="logo" style="mb-5 xl:mb-0" />
 		</a>
 		<Icon icon="logo" style="block md:hidden ml-5 w-48" />
-
+<LocaleSelector style='hidden md:block py-1 mb-5 xl:mb-0' />
+</div>
 		<!-- desktop nav -->
 		<div class="hidden space-x-10 md:flex flex-wrap justify-center items-center font-bold">
-      <LocaleSelector />
 
-			<button on:click={() => scroll(howItWorks)}>{$_('howItWorks')}</button>
-			<button on:click={() => scroll(faq)}>{$_('FAQ')}</button>
-			<button on:click={() => scroll(about)}>{$_('about')}</button>
+
+			<button on:click={() => scroll(howItWorks)}>{$_('nav.howItWorks')}</button>
+			<button on:click={() => scroll(faq)}>{$_('nav.faq')}</button>
+			<button on:click={() => scroll(about)}>{$_('nav.about')}</button>
 			{#if !$user}
 				<button class="border rounded-full px-6 py-2 font-bold" on:click={() => goto('/register')}
-					>{$_('startInSeconds')}
+					>{$_('nav.startInSeconds')}
 				</button>
 				<button
 					class="bg-black text-white border rounded-full px-6 py-2 font-bold"
 					on:click={() => goto('/login')}
 				>
-					{$_('signIn')}
+					{$_('nav.signIn')}
 				</button>
 			{:else}
 				<button
@@ -53,6 +55,7 @@
 					Sign out
 				</button>
 			{/if}
+
 		</div>
 
 		<!-- mobile nav -->
@@ -68,24 +71,24 @@
 				: 'right-[-100%]'} transition-all ease-in-out duration-300 h-[100vh] w-full bg-white"
 		>
 			<div class="space-y-8 mt-24 ml-10 font-bold text-xl">
-        <LocaleSelector />
+
 
 				<button on:click={() => mobileMenuButtonClick(howItWorks)} class="block"
-					>{$_('howItWorks')}</button
+					>{$_('nav.howItWorks')}</button
 				>
-				<button on:click={() => mobileMenuButtonClick(faq)} class="block">{$_('FAQ')}</button>
-				<button on:click={() => mobileMenuButtonClick(about)} class="block">{$_('about')}</button>
+				<button on:click={() => mobileMenuButtonClick(faq)} class="block">{$_('nav.faq')}</button>
+				<button on:click={() => mobileMenuButtonClick(about)} class="block">{$_('nav.about')}</button>
 				{#if !$user}
 					<button
 						class="border rounded-full px-6 py-2 font-bold block"
 						on:click={() => goto('/register')}
-						>{$_('signIn')}
+						>{$_('nav.startInSeconds')}
 					</button>
 					<button
 						class="bg-black text-white border rounded-full px-6 py-2 font-bold block"
 						on:click={() => goto('/login')}
 					>
-            {$_('startInSeconds')}
+            {$_('nav.signIn')}
 					</button>
 				{:else}
 					<button
@@ -100,6 +103,8 @@
 						Sign out
 					</button>
 				{/if}
+
+ <LocaleSelector style='py-1' />
 			</div>
 		</div>
 	</nav>
