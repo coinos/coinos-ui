b1843d46dd406f6f7a657ab680407644f096e2c3
Adam Soltys <asoltys@gmail.com>
2022-08-22 15:11:03 -0700

    fix login

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -4,5 +4,5 @@ export async function POST({ setHeaders, request }) {
 	let user = Object.fromEntries(await request.formData());
 	await login(user, setHeaders);
 
-	return { location: `/${data.username}/dashboard` };
+	return { location: `/${user.username}/dashboard` };
 }
