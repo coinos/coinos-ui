aca9e5105d006f49cebb6e89c3634f555e2e6740
Adam Soltys <asoltys@gmail.com>
2022-08-25 08:59:47 -0700

    fix withdrawals

--- src/routes/invoice/+server.js
+++ src/routes/invoice/+server.js
@@ -1,16 +1,12 @@
-import cookie from 'cookie';
-import { get, post } from '$lib/utils';
+import { auth, get, post } from '$lib/utils';
 
 export async function POST({ request }) {
-	let { token } = cookie.parse(request.headers.get('cookie') || '');
-
 	let network = 'lightning';
 	let { amount, currency, rate, tip, text, username } = await request.json();
-	let headers = { authorization: `Bearer ${token}` };
 
-	({ text } = await post('/lightning/invoice', { amount }, headers));
+	({ text } = await post('/lightning/invoice', { amount }, auth(request)));
 	let invoice = { amount, currency, network, rate, tip, text };
-	let { uuid } = await post('/invoice', { invoice, user: { username } }, headers);
+	let { uuid } = await post('/invoice', { invoice, user: { username } }, auth(request));
 
 	return new Response(JSON.stringify(uuid), { headers: { 'content-type': 'application/json' } });
 }
