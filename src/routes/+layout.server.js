8d09fd39f2701dbc0853f9f364eb729c67cfef5d
Adam Soltys <asoltys@gmail.com>
2022-08-29 17:10:25 -0700

    switch to custodial btc account in layout file

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,7 +1,9 @@
 import { error } from '@sveltejs/kit';
 import cookie from 'cookie';
 import { protectedRoutes } from '$lib/utils';
-import { get } from '$lib/utils';
+import { get, post, auth } from '$lib/utils';
+
+const btc = import.meta.env.VITE_BTC;
 
 export async function load({ request, setHeaders, url, params }) {
 	let { token } = cookie.parse(request.headers.get('cookie') || '');
@@ -13,6 +15,14 @@ export async function load({ request, setHeaders, url, params }) {
 		if (protectedRoutes.find((p) => url.pathname.match(p))) {
 			if (user.username !== params.username) throw error(404, 'user not found');
 		}
+
+    let account = user.accounts.find((a) => a.asset === btc && !a.pubkey);
+    if (account) {
+      let { id } = account;
+      if (user.account_id !== id) {
+        user = await post("/shiftAccount", { id }, auth(request));
+      }
+    }
 	}
 
 	rates = await get('/rates');
