c5286d71b468360b7956d0c0d6edb0132c41d956
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-09-15 18:30:57 -0600

    update jobs page and add news page

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,1 +1,19 @@
-News
+<script>
+	import { Icon, Footer } from '$comp';
+	import { t } from '$lib/translations';
+</script>
+
+<a href="/">
+	<Icon icon="logo" style="mx-auto mt-10" />
+</a>
+<div class="w-11/12 md:w-3/4 lg:w-1/2 2xl:w-1/3 max-w-3xl mx-auto my-20 space-y-5">
+	<h1 class="text-5xl font-medium">{$t('news.header')}</h1>
+
+	<h2 class="text-2xl font-medium">{$t('news.subheader')}</h2>
+
+	<p class="text-justify text-xl text-secondary">
+		{$t('news.update1')}
+	</p>
+</div>
+
+<Footer />
