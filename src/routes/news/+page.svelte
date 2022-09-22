830784acb2fe7c086ee530e0dbf25472da239c6b
Adam Soltys <asoltys@gmail.com>
2022-09-22 16:52:54 -0700

    press release

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Icon, Footer } from '$comp';
+	import { Icon, Footer, Check, Quote } from '$comp';
 	import { t } from '$lib/translations';
 </script>
 
@@ -12,7 +12,110 @@
 	<h2 class="text-2xl font-medium">{$t('news.subheader')}</h2>
 
 	<p class="text-justify text-xl text-secondary">
-		{$t('news.update1')}
+		Press Release - Coinos Redesign - September 23, 2022
+	</p>
+
+	<p>
+		Today coinos.io is launching a new and improved interface for their open source web wallet,
+		allowing businesses to accept Bitcoin as payment with no setup or merchant fees.
+	</p>
+
+	<p>
+		Coinos is a web-based payment application that is mobile and tablet friendly with handy currency
+		conversion and reporting features. Bitcoin is the number one digital currency in the world and
+		offers a secure and inexpensive way for people all over the globe to make and accept payments.
+	</p>
+
+	<blockquote
+		class="p-4 my-4 bg-gray-50 border-l-4 border-gray-300 dark:border-gray-500 dark:bg-gray-800"
+	>
+		<Quote /> Coinos has enabled businesses to accept Bitcoin since 2012, but now we have made it even
+		easier with a new simple, friendly and customizable interface for both businesses and customers to
+		use.
+		<figcaption class="flex justify-center items-center mt-6 space-x-3">
+			<img class="w-6 h-6 rounded-full" src="/images/adam.webp" alt="Adam Soltys" />
+			<div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+				<cite class="pr-3 font-medium text-gray-900 dark:text-white">Adam Soltys</cite>
+				<cite class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">Founder and CTO</cite
+				>
+			</div>
+		</figcaption>
+	</blockquote>
+
+	<p>
+		Coinos started as an indie bitcoin wallet created by adam soltys with the intention of providing
+		people with a fast, cheap, and easy way to get started with bitcoin. it's free to register an
+		account and you can sign up with just a username and password.
+	</p>
+
+	<h3 class="text-xl font-medium">Why accept Bitcoin?</h3>
+	<blockquote
+		class="p-4 my-4 bg-gray-50 border-l-4 border-gray-300 dark:border-gray-500 dark:bg-gray-800"
+	>
+		<Quote />
+		We have already brought on some local businesses, here in Vancouver, and the businesses owners are
+		excited about the opportunity coinos is giving them by attracting new customers by being able to
+		accept Bitcoin. With coino.io there are no set up fees, no merchant fees and transaction are safe
+		and secure with payments going directly to the business wallet. There is no middle person.
+		<figcaption class="flex justify-center items-center mt-6 space-x-3">
+			<img class="w-6 h-6 rounded-full" src="/images/cole.webp" alt="Adam Soltys" />
+			<div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+				<cite class="pr-3 font-medium text-gray-900 dark:text-white">Cole Goodwin</cite>
+				<cite class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400"
+					>Bitcoin Professional</cite
+				>
+			</div>
+		</figcaption>
+	</blockquote>
+
+	<p>
+		With countries adopting Bitcoin and more businesses accepting Bitcoin, enabling simple,
+		real-time payments, you do not want to miss out on the opportunity.
+	</p>
+
+	<h3 class="text-xl font-medium">Coinos Timeline</h3>
+	<ul>
+		<li class="flex items-center space-x-3">
+			<Check />
+			2012 Coding started
+		</li>
+		<li class="flex items-center space-x-3">
+			<Check />
+			2013 Used by merchants in Vancouver to accept on-chain bitcoin
+		</li>
+		<li class="flex items-center space-x-3">
+			<Check />
+			2018 Lightning network supported
+		</li>
+		<li class="flex items-center space-x-3">
+			<Check />
+			2020 Incorporated in BC and registered with Fintrac
+		</li>
+		<li class="flex items-center space-x-3">
+			<Check />
+			2021 Liquid network and asset issuance supported
+		</li>
+		<li class="flex items-center space-x-3">
+			<Check />
+			2022 10 year anniversary and UI redesign
+		</li>
+	</ul>
+
+	<h3 class="text-xl font-medium">Contacts</h3>
+
+	<p>
+		<b>Adam</b> - Founder & CTO<br />
+		<a class="hover:text-gray-500" href="mailto:adam@coinos.io ">adam@coinos.io</a>
+	</p>
+
+	<p>
+		<b>Cole</b> - Bitcoin Professional<br />
+		<a class="hover:text-gray-500" href="mailto:cole@coinos.io">cole@coinos.io</a>
+	</p>
+
+	<p>
+		<b>Shauna</b> - Operations & Marketing<br />
+		<a class="hover:text-gray-500" href="mailto:shauna@coinos.io">shauna@coinos.io</a>
 	</p>
 </div>
 
