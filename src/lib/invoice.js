59d43ba294e49cbb46ea4d2d0ddb0d22bbc7ba6e
Adam Soltys <asoltys@gmail.com>
2022-10-04 20:28:29 -0700

    tipping fixes

---
+++ src/lib/invoice.js
@@ -0,0 +1,20 @@
+import { auth, get, post } from '$lib/utils';
+import { redirect } from '@sveltejs/kit';
+
+export default async ({ cookies, request }) => {
+	let form = await request.formData();
+
+	let invoice = {
+		amount: parseInt(form.get('amount')),
+		tip: parseInt(form.get('tip')),
+		network: 'lightning',
+		prompt: form.get('prompt') === 'true',
+		rate: await get('/rate')
+	};
+
+	let user = { username: form.get('username') };
+
+	let { uuid } = await post('/invoice', { invoice, user }, auth(cookies));
+
+	throw redirect(307, `/invoice/${uuid}`);
+};
