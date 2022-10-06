a1d94cf39f39bbcc51d6b7a5aff85990d2f430e1
Adam Soltys <asoltys@gmail.com>
2022-10-06 10:04:29 -0700

    remove asset from bitcoin send

--- src/routes/send/bitcoin/[address]/+page.server.js
+++ src/routes/send/bitcoin/[address]/+page.server.js
@@ -4,7 +4,7 @@ import { auth, post } from '$lib/utils';
 export const actions = {
 	default: async ({ cookies, request }) => {
 		let { address, amount } = Object.fromEntries(await request.formData());
-		let { tx } = await post('/bitcoin/fee', { address, amount, asset }, auth(cookies));
+		let { tx } = await post('/bitcoin/fee', { address, amount }, auth(cookies));
 		await post('/bitcoin/send', { address, tx }, auth(cookies));
 		throw redirect(307, '/sent');
 	}
