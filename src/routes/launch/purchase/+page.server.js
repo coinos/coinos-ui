9d0f05923bbd23cc314dd4746c6a820bab2c8f7e
Adam Soltys <asoltys@gmail.com>
2022-10-13 18:04:14 -0700

    tickets

--- src/routes/launch/purchase/+page.server.js
+++ src/routes/launch/purchase/+page.server.js
@@ -1,8 +1,26 @@
-export const load = ({ parent }) => parent();
+import { auth, get, post } from '$lib/utils';
+import tickets from "$lib/tickets";
+
+export const load = () => get('/ticket');
 
 export const actions = {
 	default: async ({ cookies, request }) => {
-		let form = Object.fromEntries(await request.formData());
-		console.log(form);
+		let { ticket } = Object.fromEntries(await request.formData());
+
+		let r = await post(
+			'/assets',
+			{
+				name: 'Launch Party Ticket',
+				ticker: 'LPT',
+				precision: 0,
+				asset_amount: '1',
+				token_amount: 0,
+				domain: 'coinos.io',
+        filename: tickets[ticket],
+			},
+			auth(cookies)
+		);
+
+		console.log(r);
 	}
 };
