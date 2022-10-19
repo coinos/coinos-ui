4e03d33c6c28baccd2b052e7c4bf2620cf68a7ee
Adam Soltys <asoltys@gmail.com>
2022-10-18 22:54:37 -0700

    link to launch party in announcement bar

--- src/routes/launch/ticket/+page.svelte
+++ src/routes/launch/ticket/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	export let data;
-	let { ticket } = data;
+	let { asset, ticket } = data;
 </script>
 
 <div class="mx-auto h-screen flex items-center justify-center px-4">
@@ -8,6 +8,13 @@
 		<h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-800 text-center">
 			Your Ticket
 		</h1>
+
+		<p>
+			We've minted your ticket as an <a
+				href={`https://blockstream.info/liquid/asset/${asset}`}
+				class="underline">NFT on the Liquid Blockchain</a
+			> as a proof of purchase and commemorative token.
+		</p>
 
 		<img
 			src={`/tickets/${(ticket + 1).toString().padStart(2, '0')}.png`}
