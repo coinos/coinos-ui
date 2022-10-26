c74b2a1fb9ad69ffcb9a0c0e2aa2931a98b22146
Adam Soltys <asoltys@gmail.com>
2022-10-26 15:44:35 -0700

    regular pin input for now

--- src/components/Pin.svelte
+++ src/components/Pin.svelte
@@ -1,28 +1,16 @@
 <script>
-	import { tick } from 'svelte';
 	import { pin } from '$lib/store';
-	import { Pincode, PincodeInput } from 'svelte-pincode';
-	let pinCode = [];
+	let pinCode = '';
 	let pinInput;
 
-  $: pinCode && pinInput && tick().then(pinInput && pinInput.focusNextEmptyInput());
-	$: pinCode.length > 5 && ($pin = pinCode.join(''));
+	$: console.log(pinCode.length);
+	$: pinCode.length > 5 && ($pin = pinCode);
 </script>
 
 <div class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20">
 	<div class="relative top-1/3 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white space-y-5">
 		<h1 class="text-center text-2xl font-semibold">Please enter your PIN</h1>
-		<div class="text-center">
-			<Pincode bind:code={pinCode} bind:this={pinInput}>
-				<PincodeInput />
-				<PincodeInput />
-				<PincodeInput />
-				<PincodeInput />
-				<PincodeInput />
-				<PincodeInput />
-			</Pincode>
-		</div>
-
+		<input bind:value={pinCode} />
 		<div class="w-full flex">
 			<button
 				type="submit"
