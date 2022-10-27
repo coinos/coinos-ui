dfd94014fb744dd67fef15f71980dcda20cef7eb
Adam Soltys <asoltys@gmail.com>
2022-10-27 07:06:50 -0700

    fix user profile page

--- src/components/AppHeader.svelte
+++ src/components/AppHeader.svelte
@@ -12,12 +12,13 @@
 	$: update(data);
 	let update = (data) => ({ subject, user } = data);
 
-	$: user && (menuButtons = [
+  let menuButtons;
+	if(user) menuButtons = [
 		{ key: 'nav.profile', icon: 'profile', href: `/${user.username}` },
 		{ key: 'nav.settings', icon: 'settings', href: `/${user.username}/settings` },
 		{ key: 'nav.support', icon: 'support', href: `/${user.username}/support` },
 		{ key: 'nav.signOut', icon: 'logout', href: `/logout` }
-	]);
+	];
 
 	let showMenu = false;
 
