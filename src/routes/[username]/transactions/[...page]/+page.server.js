2b04d2030f6303ecf080910d2a6b60378c0ebbca
Adam Soltys <asoltys@gmail.com>
2022-11-01 11:43:39 -0700

    set default payment range to a day

--- src/routes/[username]/transactions/[...page]/+page.server.js
+++ src/routes/[username]/transactions/[...page]/+page.server.js
@@ -12,9 +12,9 @@ export async function load({ cookies, params, request }) {
 	if (!parseInt(page)) page = 1;
 
 	let offset = (page - 1) * limit;
+  if (!start) start = Date.now() - 24 * 60 * 60 * 1000;
 
-	let url = `/payments?v2=true&limit=${limit}&offset=${offset}`;
-	if (start) url += `&start=${start}`;
+	let url = `/payments?v2=true&start=${start}&limit=${limit}&offset=${offset}`;
 	if (end) url += `&end=${end}`;
 
 	let { total, transactions } = await get(url, auth(cookies));
