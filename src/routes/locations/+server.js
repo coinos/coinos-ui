c12857a38eed1227ed51dc6e0af7242e66dd7561
Adam Soltys <asoltys@gmail.com>
2022-11-02 16:37:34 -0700

    locations endpoint

---
+++ src/routes/locations/+server.js
@@ -0,0 +1,7 @@
+import { get } from "$lib/utils";
+import { json } from '@sveltejs/kit';
+
+export async function GET({ fetch, setHeaders }) {
+	setHeaders({ 'cache-control': 'public, max-age=600' });
+	return json(await get('/locations'));
+}
