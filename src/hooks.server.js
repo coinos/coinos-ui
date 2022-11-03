5d3618a4397f88ed37cd25f39261c3a2bcc2aab7
Adam Soltys <asoltys@gmail.com>
2022-11-02 22:45:38 -0700

    move route checks to hooks

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,20 +2,23 @@ import { auth, get, protectedRoutes } from '$lib/utils';
 
 export async function handle({ event, resolve }) {
 	let token = event.cookies.get('token');
-	let { params, url } = event;
+	let { params, url: { pathname, origin } } = event;
 
 	let user;
-	if (token) {
+	if (token && pathname !== '/logout') {
 		try {
 			user = await get('/me', auth(event.cookies));
 			event.locals.user = user;
+
+      if (['/login', '/register'].includes(pathname))
+        return Response.redirect(origin + `/${user.username}/dashboard`, 307);
 		} catch (e) {
-			console.log('failed to get user', e);
+      return Response.redirect(origin + '/logout', 307);
 		}
 	}
 
-	if (protectedRoutes.find((p) => url.pathname.match(p))) {
-		if (user?.username !== params.username) return Response.redirect(url.origin + '/login', 307);
+	if (protectedRoutes.find((p) => pathname.match(p))) {
+		if (user?.username !== params.username) return Response.redirect(origin + '/login', 307);
 	}
 
 	const response = await resolve(event);
--- src/routes/(app)/+layout.server.js
+++ src/routes/(app)/+layout.server.js
@@ -1,5 +1,4 @@
 import { error, redirect } from '@sveltejs/kit';
-import { protectedRoutes } from '$lib/utils';
 import { get, post, auth } from '$lib/utils';
 import { env } from '$env/dynamic/public';
 
@@ -8,20 +7,11 @@ export const { PUBLIC_BTC: btc } = env;
 export async function load({ cookies, locals, request, url, params }) {
 	url.pathname;
 	let token = cookies.get('token');
-	let user;
 	let rates;
 	let subject;
 
-	if (token && token !== 'undefined') {
-		user = locals.user;
-
-		if (protectedRoutes.find((p) => url.pathname.match(p))) {
-			if (user.username !== params.username) throw redirect(307, '/login');
-		}
-
-		if (['/login', '/register'].includes(url.pathname))
-			throw redirect(307, `/${user.username}/dashboard`);
-
+	let { user } = locals;
+	if (user) {
 		let account = user.accounts.find((a) => a.asset === btc && !a.pubkey);
 		if (account) {
 			let { id } = account;
