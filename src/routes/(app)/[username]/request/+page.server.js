ade26b888e0772968306e9b19e1cec322046d21b
Adam Soltys <asoltys@gmail.com>
2022-11-03 23:40:03 -0700

    set request user and recipient

--- src/routes/(app)/[username]/request/+page.server.js
+++ src/routes/(app)/[username]/request/+page.server.js
@@ -1,7 +1,12 @@
-import { auth, post } from "$lib/utils";
+import { auth, post } from '$lib/utils';
+
+export let load = ({ params }) => {
+  let { username } = params;
+} 
+
 export const actions = {
 	default: async ({ cookies, request }) => {
 		let form = Object.fromEntries(await request.formData());
-    return { request: await post('/requests', form, auth(cookies)) };
+		return { request: await post('/requests', form, auth(cookies)) };
 	}
 };
