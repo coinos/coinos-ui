64865d202bc30bfadcf99b309100d8194544cf0d
Adam Soltys <asoltys@gmail.com>
2022-11-04 12:44:10 -0700

    menu buttons

--- src/routes/(app)/[username]/request/+page.server.js
+++ src/routes/(app)/[username]/request/+page.server.js
@@ -1,3 +1,4 @@
+import { redirect } from "@sveltejs/kit";
 import { auth, post } from '$lib/utils';
 
 export let load = ({ params }) => {
@@ -5,8 +6,9 @@ export let load = ({ params }) => {
 } 
 
 export const actions = {
-	default: async ({ cookies, request }) => {
+	default: async ({ cookies, request, url }) => {
 		let form = Object.fromEntries(await request.formData());
-		return { request: await post('/requests', form, auth(cookies)) };
+		await post('/requests', form, auth(cookies));
+    throw redirect(307, url.href + '/sent');
 	}
 };
