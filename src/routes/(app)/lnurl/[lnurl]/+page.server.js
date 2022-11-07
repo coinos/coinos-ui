7e8d2f372a005a15e271748209034ff65d2f4946
Adam Soltys <asoltys@gmail.com>
2022-11-06 22:30:53 -0800

    fix redirects

--- src/routes/(app)/lnurl/[lnurl]/+page.server.js
+++ src/routes/(app)/lnurl/[lnurl]/+page.server.js
@@ -2,7 +2,7 @@ import { auth, get, post } from '$lib/utils';
 import { error, redirect } from '@sveltejs/kit';
 
 export async function load({ locals, params }) {
-	if (!locals.user) redirect(307, '/login');
+	if (!locals.user) throw redirect(307, '/login');
 	let { lnurl } = params;
 	lnurl = await get(`/decode?text=${lnurl}`);
 	if (lnurl.tag !== 'payRequest') throw error(500, 'We can only handle lnurl pay requests');
