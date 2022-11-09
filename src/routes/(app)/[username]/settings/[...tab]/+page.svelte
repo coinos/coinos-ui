305f26ccefffe52b3fb65995e8caf437f1a92057
Adam Soltys <asoltys@gmail.com>
2022-11-09 10:53:41 -0800

    settings fixes

--- src/routes/(app)/[username]/settings/[...tab]/+page.svelte
+++ src/routes/(app)/[username]/settings/[...tab]/+page.svelte
@@ -1,109 +1,109 @@
 <script>
-  import { onMount } from 'svelte';
-  import { fly } from 'svelte/transition';
-  import { enhance } from '$app/forms';
+	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
+	import { enhance } from '$app/forms';
 
-  import { Icon, Spinner, Pin } from '$comp';
-  import { t } from '$lib/translations';
-  import { failure, success } from '$lib/utils';
-  import { avatarUpload, bannerUpload, pin } from '$lib/store';
-  import { upload } from '$lib/upload';
-  import { page } from '$app/stores';
+	import { Icon, Spinner, Pin } from '$comp';
+	import { t } from '$lib/translations';
+	import { failure, success } from '$lib/utils';
+	import { avatarUpload, bannerUpload, pin } from '$lib/store';
+	import { upload } from '$lib/upload';
+	import { page } from '$app/stores';
 
-  import Account from './_account.svelte';
-  import Pos from './_pos.svelte';
-  import Security from './_security.svelte';
+	import Account from './_account.svelte';
+	import Pos from './_pos.svelte';
+	import Security from './_security.svelte';
 
-  export let data;
-  export let form;
+	export let data;
+	export let form;
 
-  let submit;
+	let submit;
 
-  let { user, token, tab } = data;
-  console.log("TAB", tab)
+	let { user, token, tab } = data;
 
-  $: update(data);
-  let update = () => ({ user, token } = data);
+	$: update(data);
+	let update = () => ({ user, token, tab } = data);
 
-  $: form?.user && ({ user } = form);
+	$: form?.user && ({ user } = form);
 
-  // $: if (form?.message?.startsWith('Pin')) {
-  // 	failure('Wrong pin, try again');
-  // 	$pin = '';
-  // }
+	$: form?.success && success('Settings saved!');
 
-  let tabs = [
-    { name: 'account', key: 'ACCOUNT', comp: Account },
-    { name: 'pos', key: 'POINT_OF_SALE', comp: Pos },
-    { name: 'security', key: 'SECURITY', comp: Security }
-  ];
+	// $: if (form?.message?.startsWith('Pin')) {
+	// 	failure('Wrong pin, try again');
+	// 	$pin = '';
+	// }
 
-  $: ({ comp } = tabs.find((t) => t.name === tab));
+	let tabs = [
+		{ name: 'account', key: 'ACCOUNT', comp: Account },
+		{ name: 'pos', key: 'POINT_OF_SALE', comp: Pos },
+		{ name: 'security', key: 'SECURITY', comp: Security }
+	];
 
-  let loading;
-  let save = async (msg) => {
-    loading = true;
+	$: ({ comp } = tabs.find((t) => t.name === tab));
 
-    try {
-      if ($avatarUpload) {
-        await upload($avatarUpload.file, $avatarUpload.type, $avatarUpload.progress, token);
-      }
+	let loading;
+	let save = async (msg) => {
+		loading = true;
 
-      if ($bannerUpload) {
-        await upload($bannerUpload.file, $bannerUpload.type, $bannerUpload.progress, token);
-      }
-    } catch (e) {
-      console.log(e);
-      failure('Something went wrong');
-    }
+		try {
+			if ($avatarUpload) {
+				await upload($avatarUpload.file, $avatarUpload.type, $avatarUpload.progress, token);
+			}
 
-    loading = false;
-  };
+			if ($bannerUpload) {
+				await upload($bannerUpload.file, $bannerUpload.type, $bannerUpload.progress, token);
+			}
+		} catch (e) {
+			console.log(e);
+			failure('Something went wrong');
+		}
 
-  let loaded;
-  onMount(() => setTimeout(() => (loaded = true), 50));
+		loading = false;
+	};
+
+	let loaded;
+	onMount(() => setTimeout(() => (loaded = true), 50));
 </script>
 
 {#if loaded && user.haspin && $pin?.length !== 6}
-  <Pin />
+	<Pin />
 {/if}
 
 <form method="POST" class="mb-[154px]" use:enhance on:submit={save}>
-  <input type="hidden" name="pin" value={$pin} />
+	<input type="hidden" name="pin" value={$pin} />
 
-  <div class="mt-24 mb-20 px-3 md:px-0 w-full md:w-[400px] mx-auto space-y-8">
-    <h1 class="text-center text-3xl md:text-4xl font-semibold mb-10">
-      {$t('user.settings.header')}
-    </h1>
+	<div class="mt-24 mb-20 px-3 md:px-0 w-full md:w-[400px] mx-auto space-y-8">
+		<h1 class="text-center text-3xl md:text-4xl font-semibold mb-10">
+			{$t('user.settings.header')}
+		</h1>
 
-    <div class="font-bold flex justify-between items-center border-b pb-3 text-secondary">
-      {#each tabs as { name, key }}
-        <a href={`/${user.username}/settings/${name}`}>
-          <button class="hover:opacity-80" class:text-black={tab === name}
-            >{$t(`user.settings.${key}`)}</button
-          >
-        </a>
-      {/each}
-    </div>
+		<div class="font-bold flex justify-between items-center border-b pb-3 text-secondary">
+			{#each tabs as { name, key }}
+				<a href={`/${user.username}/settings/${name}`}>
+					<button class="hover:opacity-80" class:text-black={tab === name}
+						>{$t(`user.settings.${key}`)}</button
+					>
+				</a>
+			{/each}
+		</div>
 
-    <svelte:component this={comp} {user} {submit} />
-  </div>
+		<svelte:component this={comp} {user} {submit} />
+	</div>
 
-  <div
-    class="z-10 fixed bottom-0 bg-white shadow border-t w-full flex justify-center items-center py-3"
-  >
-    <button
-      bind:this={submit}
-      type="submit"
-      class="border-2 border-black rounded-xl font-semibold mx-auto py-3 w-40 hover:opacity-80"
-      class:bg-black={loading}
-    >
-      {#if loading}
-        <Spinner />
-      {:else}
-        <div class="my-auto">Save Settings</div>
-      {/if}
-    </button>
-  </div>
+	<div
+		class="z-10 fixed bottom-0 bg-white shadow border-t w-full flex justify-center items-center py-3"
+	>
+		<button
+			bind:this={submit}
+			type="submit"
+			class="border-2 border-black rounded-xl font-semibold mx-auto py-3 w-40 hover:opacity-80"
+			class:bg-black={loading}
+		>
+			{#if loading}
+				<Spinner />
+			{:else}
+				<div class="my-auto">Save Settings</div>
+			{/if}
+		</button>
+	</div>
 </form>
-
--- src/routes/(app)/disable2fa/+server.js
+++ src/routes/(app)/disable2fa/+server.js
@@ -1,8 +1,13 @@
+import { error } from '@sveltejs/kit';
 import { auth, post } from '$lib/utils';
 
-export async function PUT({ cookies, request }) {
-	let body = await request.json();
-	let res = await post('/disable2fa', body, auth(cookies));
+export async function POST({ cookies, request }) {
+	try {
+		let body = await request.json();
+		let res = await post('/disable2fa', body, auth(cookies));
 
-	return new Response(JSON.stringify(res));
+		return new Response(JSON.stringify(res));
+	} catch (e) {
+		throw error(500, 'Problem disabling 2fa');
+	}
 }
