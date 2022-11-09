5a6817950d0da622170fcb06b9de909a5baba24d
Adam Soltys <asoltys@gmail.com>
2022-11-09 15:21:00 -0800

    nicer error page

--- src/routes/(app)/+error.svelte
+++ src/routes/(app)/+error.svelte
@@ -6,21 +6,36 @@
 
 	export let data;
 	let { user } = data;
+
+	$: message = $page.error.message;
+	$: if (message.startsWith('Internal')) message = 'Something went wrong';
 </script>
 
 {#if $page.error.message.includes('not found') || $page.status === 404}
 	<PageNotFound />
 {:else}
-	<div class="container px-4 max-w-md mx-auto mt-20 text-center">
-		<h1 class="text-center text-3xl md:text-4xl font-semibold mb-8">
+	<div class="container px-4 max-w-lg mx-auto mt-20 space-y-5">
+		<div class="w-[243px] mx-auto mb-10">
+			<a href="/">
+				<Icon icon="logo" />
+			</a>
+		</div>
+
+	<div class="flex justify-center items-center">
+		<div class="shadow-xl rounded-3xl p-10 pb-12 space-y-5 w-full mx-5 md:mx-0 md:w-[400px]">
+		<h1 class="text-3xl md:text-4xl font-semibold">
 			{$t('error.header')}
 		</h1>
 
-		<p class="px-1 break-all mb-8">{$page.error.message}</p>
+		<p class="text-secondary">
+    {message}. If the problem persists, please <a href="/support" class="underline">let us know.</a>
+		</p>
 
 		<button
 			class="rounded-full bg-black text-white py-2 px-5 font-bold hover:opacity-80 mb-2"
 			on:click={back}>Back</button
 		>
+    </div>
+    </div>
 	</div>
 {/if}
