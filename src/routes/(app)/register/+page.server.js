64284b65214f049f62c4ac7b123a64a164ecc5bf
Adam Soltys <asoltys@gmail.com>
2022-11-09 22:52:21 -0800

    redirect if logged in

--- src/routes/(app)/register/+page.server.js
+++ src/routes/(app)/register/+page.server.js
@@ -1,6 +1,11 @@
 import { invalid, redirect } from '@sveltejs/kit';
 import { post, login } from '$lib/utils';
 
+export const load = async ({ parent }) => {
+  let { user } = await parent();
+  if (user) throw redirect(307, '/dashboard');
+} 
+
 export const actions = {
 	default: async ({ cookies, request }) => {
 		let form = Object.fromEntries(await request.formData());
