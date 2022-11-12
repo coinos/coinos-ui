d668a5976f5c35f352247ae529c22eafd2e53c78
Adam Soltys <asoltys@gmail.com>
2022-11-11 22:01:16 -0800

    metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,29 @@
 <script>
 	import '../app.css';
+	import { page } from '$app/stores';
+
+	let { host, pathname } = $page.url;
 </script>
 
 <svelte:head>
 	<title>coinos</title>
-	<meta property="og:image" content="/icons/logo.svg" />
+	<meta property="og:title" content="coinos" />
+	<meta name="twitter:title" content="coinos" />
+
+	<meta property="og:image" content={`${host}/icons/logo.svg`} />
+
+	<meta property="og:type" content="website" />
+	<meta property="og:description" content="An easy to use bitcoin web wallet" />
+	<meta name="description" content="An easy to use bitcoin web wallet" />
+
+	<meta name="keywords" content="coinos easy bitcoin web wallet" />
+
+	<meta name="twitter:image" content={`${host}/icons/logo.svg`} />
+	<meta name="twitter:card" content="summary_large_image" />
+
+	<meta property="og:url" content={host + pathname} />
+	<meta name="twitter:site" content="@coinoswallet" />
+	<meta name="twitter:creator" content="@coinoswallet" />
 </svelte:head>
 
 <slot />
