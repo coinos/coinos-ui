2eb9b1d12c05d4f3d2ac96fc8053b4fa0bd37ba5
Adam Soltys <asoltys@gmail.com>
2022-11-12 03:31:54 -0800

    requests flow

--- src/routes/(app)/[username]/request/[id]/+page.server.js
+++ src/routes/(app)/[username]/request/[id]/+page.server.js
@@ -1,3 +1,19 @@
 import { auth, get } from '$lib/utils';
+import { redirect } from '@sveltejs/kit';
 
-export let load = async ({ cookies, params }) => get(`/request/${params.id}`, auth(cookies));
+export let load = async ({ cookies, depends, locals: { user }, params }) => {
+	depends('app:invoice');
+	if (!user) throw redirect(307, '/login');
+
+	let { request } = await get(`/request/${params.id}`, auth(cookies));
+	let { invoice } = request;
+	if (invoice) {
+		let { amount, received, uuid } = invoice;
+		if ((received && !amount) || received >= amount) throw redirect(307, `/send/${uuid}`);
+
+		if (request.requester.username === user.username)
+			throw redirect(307, `/send/${request.invoice.uuid}`);
+	}
+
+	return { request };
+};
