dde54bbb000bdd1f6cc5be7d211d8d967d45fa49
Adam Soltys <asoltys@gmail.com>
2022-11-12 16:26:38 -0800

    fix voucher qr codes

--- src/routes/(app)/voucher/[code]/+page.server.js
+++ src/routes/(app)/voucher/[code]/+page.server.js
@@ -3,6 +3,6 @@ import Qr from 'qrcode-base64';
 export let load = async ({ params: { code }, url }) => {
 	let payment = await get(`/payment/${code}`);
 
-	let src = Qr.drawImg(url.hostname, { size: 300 });
+	let src = Qr.drawImg(`${url.hostname}/voucher/${code}`, { size: 300 });
 	return { payment, src };
 };
