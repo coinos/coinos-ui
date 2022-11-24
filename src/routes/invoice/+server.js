8aa172a566a6ddeee34c763eaed759999246242a
John Barratt <johnnybarratt@gmail.com>
2022-11-24 14:27:39 -0800

    commit before push

---
+++ src/routes/invoice/+server.js
@@ -0,0 +1,12 @@
+import { auth, get, post } from '$lib/utils';
+
+export async function POST({ request }) {
+	let network = 'lightning';
+	let { amount, currency, rate, tip, text, username } = await request.json();
+	({ text } = await post('/lightning/invoice', { amount }, auth(request)));
+	console.log("reqest", text);
+	let invoice = { amount, currency, network, rate, tip, text };
+	let { uuid } = await post('/invoice', { invoice, user: { username } }, auth(request));
+
+	return new Response(JSON.stringify(uuid), { headers: { 'content-type': 'application/json' } });
+}
