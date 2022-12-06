e4c5f6045587616bc722e92561c39595acdfca76
John Barratt <johnnybarratt@gmail.com>
2022-12-05 17:05:25 -0800

    add responsive design for header

--- src/components/LandingHero.svelte
+++ src/components/LandingHero.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="mt-[96.88px] md:mt-0">
-	<div class="space-y-10">
+	<div class="space-y-10 pt-28 md:pt-0">
 		<h1
 			class="text-5xl md:text-6xl lg:text-7xl font-medium text-center w-full md:w-3/4 lg:w-1/2 max-w-7xl mx-auto leading-tight"
 		>
