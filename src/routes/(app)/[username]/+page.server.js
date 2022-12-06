55a818c6e0330135c1b3e7994e7e11e70b03b034
Adam Soltys <asoltys@gmail.com>
2022-12-06 11:40:01 -0800

    URI encode username for lightning address

--- src/routes/(app)/[username]/+page.server.js
+++ src/routes/(app)/[username]/+page.server.js
@@ -1,7 +1,7 @@
 import Qr from 'qrcode-base64';
 
 export async function load({ params, url }) {
-	let text = `${params.username}@${url.hostname}`;
+	let text = `${encodeURI(params.username)}@${url.hostname}`;
 	let src = Qr.drawImg(text, { size: 300 });
 	return { src, text };
 }
