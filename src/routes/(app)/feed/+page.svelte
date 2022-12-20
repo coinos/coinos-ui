f6bb653a534d26478b8529d6ea7b073782d6a102
Adam Soltys <asoltys@gmail.com>
2022-12-20 12:38:27 -0800

    mobile style

--- src/routes/(app)/feed/+page.svelte
+++ src/routes/(app)/feed/+page.svelte
@@ -19,27 +19,26 @@
 </script>
 
 <div class="mt-24 mb-20">
-	<h1 class="px-3 md:px-0 text-center text-3xl md:text-4xl font-semibold mb-10">Nostr</h1>
-
 	<div class="container w-full mx-auto text-lg px-4 max-w-2xl space-y-5">
 		<div class="vs">
 			{#if browser}
 				<VirtualScroll data={sorted} key="id" let:data pageMode={true}>
-					<div class="grid grid-cols-3 border-b pb-5" :key={data.id} use:fade>
-						<div class="my-auto text-secondary">
-							<div class="flex">
-								<div class="my-auto">
-									<Avatar size={20} src={`/api/public/runners/${runner()}`} />
-								</div>
-								<div class="my-auto ml-1">
-									{data.pubkey.substr(0, 6)}<br />
-									<span class="text-sm">{format(new Date(data.seen * 1000), 'h:mm aaa')}</span>
-								</div>
+					<div class="flex border-b py-4" :key={data.id} use:fade>
+						<div class="mb-auto mr-2">
+							<div class="md:hidden">
+								<Avatar size={12} src={`/api/public/runners/${runner()}`} />
+							</div>
+							<div class="hidden md:block">
+								<Avatar size={20} src={`/api/public/runners/${runner()}`} />
 							</div>
 						</div>
 
-						<div class="my-auto col-span-2">
-							<div class="text-secondary break-words">{data.content}</div>
+						<div class="text-secondary break-words w-full">
+							<div class="w-full flex text-sm pb-1">
+								<div class="text-black">{data.pubkey.substr(0, 6)}</div>
+								<div class="ml-auto text-sm">{format(new Date(data.seen * 1000), 'h:mm aaa')}</div>
+							</div>
+							{data.content}
 						</div>
 					</div>
 				</VirtualScroll>
