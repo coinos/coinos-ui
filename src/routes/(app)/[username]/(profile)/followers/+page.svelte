72df415168f7a5a1ee72967a27305411de998a10
Adam Soltys <asoltys@gmail.com>
2022-12-29 10:52:04 -0800

    link to pubkeys

--- src/routes/(app)/[username]/(profile)/followers/+page.svelte
+++ src/routes/(app)/[username]/(profile)/followers/+page.svelte
@@ -14,11 +14,13 @@
 <svelte:window bind:innerWidth={w} />
 
 <div class="mt-24 mb-20 px-3 md:px-0 w-full md:w-[400px] mx-auto space-y-8">
+	<h1 class="px-3 md:px-0 text-center text-3xl md:text-4xl font-semibold">Followers</h1>
+
 	{#if browser}
 		{#if followers.length}
 			<VirtualScroll data={followers} key="pubkey" let:data pageMode={true}>
-				<a href={`/${data.username}`}>
-					<div class="flex border-b py-4 text-sm lg:text-lg text-secondary" :key={data.pubkey}>
+				<a href={`/${data.pubkey}`}>
+					<div class="flex border-b last:border-b-0 py-4 text-sm lg:text-lg text-secondary" :key={data.pubkey}>
 						<div class="mb-auto mr-2">
 							<div class="md:hidden">
 								<Avatar size={12} user={data} disabled={true} />
