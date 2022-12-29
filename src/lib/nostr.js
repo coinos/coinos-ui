f3dfdf139386f2bfda836e71711897841c85863e
Adam Soltys <asoltys@gmail.com>
2022-12-28 23:09:28 -0800

    rm feed from homepage

--- src/lib/nostr.js
+++ src/lib/nostr.js
@@ -27,22 +27,15 @@ export let sign = async ({ event, user }) => {
 	await wait(() => !!get(pw));
 	password = get(pw);
 
-	console.log('got pw');
-
 	let mnemonic, key, seed, entropy, child, privkey;
-  let stretched = await stretch(password, Buffer.from(salt, 'hex'));
-  console.log("stretched", stretched)
-  if (username ==="bob") console.log(cipher, salt, password, decode(cipher, 180));
 	entropy = Buffer.from(
 		await crypto.subtle.decrypt(
 			{ name: 'AES-GCM', iv: new Uint8Array(16) },
-			stretched,
+			await stretch(password, Buffer.from(salt, 'hex')),
 			Uint8Array.from(fromWords(decode(cipher, 180).words))
 		),
 		'hex'
 	).toString('hex');
-
-	console.log('decoded entropy');
 
 	mnemonic = entropyToMnemonic(entropy);
 	seed = mnemonicToSeedSync(mnemonic);
@@ -50,12 +43,8 @@ export let sign = async ({ event, user }) => {
 	child = key.derivePath("m/44'/1237'/0'/0/0");
 	privkey = child.privateKey;
 
-	console.log('derived privkey');
-
 	event.id = await calculateId(event);
-	console.log('got id');
 	event.sig = await signId(privkey, event.id);
-	console.log('signed');
 };
 
 export let send = (event) => {
