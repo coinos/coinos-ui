a657fab89a7de41f0a33389023166a6f7af6dbe6
Adam Soltys <asoltys@gmail.com>
2023-01-03 00:22:30 -0800

    generate pubkey on register

--- src/routes/(app)/+layout.server.js
+++ src/routes/(app)/+layout.server.js
@@ -1,18 +1,35 @@
 import { error, redirect } from '@sveltejs/kit';
-import { get, post, auth } from '$lib/utils';
+import { get, post, auth, protectedRoutes } from '$lib/utils';
 import { PUBLIC_BTC as btc } from '$env/static/public';
 
-export async function load({ cookies, locals, request, url, params }) {
-	url.pathname;
+export async function load({ cookies, request, url, params }) {
+	let { pathname } = url;
 	let token = cookies.get('token');
 	let rate,
 		rates = { USD: 1 },
 		subject;
 
-	let { user } = locals;
+	let user;
+	if (token) {
+		try {
+			user = await get('/me', auth(cookies));
+		} catch (e) {}
+	}
+
+	if (user && ['/', '/login', '/register'].includes(pathname) && request.method === 'GET')
+		throw redirect(307, `/${user.username}/dashboard`);
+
+	if (
+		protectedRoutes.find((p) => pathname.match(p)) &&
+		(!user || user.username !== params.username)
+	) {
+		throw redirect(307, '/login');
+	}
+
 	if (user) {
-		if (!user.pubkey && !url.pathname.includes('gen'))
+		if (!user.pubkey && !url.pathname.includes('gen')) {
 			throw redirect(307, `/${user.username}/generate`);
+		}
 		let account = user.accounts.find((a) => a.asset === btc && !a.pubkey);
 		if (account) {
 			let { id } = account;
