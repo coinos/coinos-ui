fc2f98b42fbe8bc76140d44c172b4c93c53d9e70
Adam Soltys <asoltys@gmail.com>
2023-02-08 14:48:26 -0800

    bounties

--- src/routes/(static)/bounties/+page.svelte
+++ src/routes/(static)/bounties/+page.svelte
@@ -1,1 +1,89 @@
-hi
+<div class="space-y-5">
+	<h1 class="text-3xl mb-6 pt-20">Bounties</h1>
+
+	<p class="text-secondary">
+		We'd like to see more on-the-ground merchant adoption in our hometown of Vancouver so we're
+		offering <b>⚡️1,000,000</b> to the first 10 people that help us onboard businesses in the following
+		categories:
+	</p>
+
+	<ul class="text-xl ml-20">
+		<li>Grocery or Produce Store</li>
+		<li>Cafe</li>
+		<li>Bakery</li>
+		<li>Butcher Shop</li>
+		<li>Laundromat or Dry Cleaner</li>
+		<li>Dollar Store, Book Store, or Gift Shop</li>
+		<li>Sushi Restaurant</li>
+		<li>Pizza by the Slice Joint</li>
+		<li>Liquor Store or Gas Station</li>
+		<li>Bubble Tea or Ice Cream Parlour</li>
+	</ul>
+
+	<p>
+		The business owners can expect free ongoing support and promotion from us and our community.
+		We'll consult with them and make sure they're all trained up and ready to go then we'll help get
+		the word out.
+	</p>
+
+	<h2 class="text-2xl">Terms and Conditions</h2>
+
+	<ul class="text-xl ml-20">
+		<li>The business must have a physical location that takes walk-in customers</li>
+		<li>They must be located in Metro Vancouver</li>
+		<li>They must have regular hours and a website with contact info</li>
+		<li>They must have a 3+ star rating on Google Maps</li>
+		<li>
+			It must be possible to pay for any of their usual goods/services with bitcoin at any time
+			during their posted hours without needing to make any special arrangements, regardless of who
+			is on staff that day.
+		</li>
+		<li>They must use Coinos and accept lightning payments</li>
+	</ul>
+
+	<p>
+		The prize money will be offered to the person who makes the referral/introduction. Whether you
+		choose to split some with the business owner is up to you.
+	</p>
+
+	<p>
+		<b>⚡️250,000</b> will be awarded once the business has met with us and is ready to take payments
+	</p>
+	<p>
+		<b>⚡️750,000</b> will be awarded once the merchant has accepted bitcoin for 6 months. We'll be
+		sending
+		<a href="https://btcmap.org/leaderboard" class="text-black hover:underline" target="_blank"
+			>shadowy supertaggers</a
+		> to check up on them periodically.
+	</p>
+
+	<p>
+		Bounties will be awarded solely at our discretion, we reserve the right to refuse any applicants
+		without cause
+	</p>
+
+	<h2 class="text-2xl">Let's go!</h2>
+
+	<p>
+		To make a referral, reach out on one of our socials or email us at <a
+			href="mailto:bounties@coinos.io"
+			class="text-black hover:underline">bounties@coinos.io</a
+		> and we'll set up an appointment with you and the business owner to get started.
+	</p>
+
+	<p>
+		We'll update this page as the bounties are claimed so you'll be able to see which ones are still
+		available.
+	</p>
+
+	<p>Thanks for participating and helping to orange pill Vancouver!</p>
+</div>
+
+<style>
+	p {
+		@apply text-secondary;
+	}
+	b {
+		@apply text-black text-xl;
+	}
+</style>
