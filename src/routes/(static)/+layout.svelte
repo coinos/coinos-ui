6790cc3d28b3d412d52293771047f342530a5499
Adam Soltys <asoltys@gmail.com>
2023-02-08 14:48:26 -0800

    bounties

--- src/routes/(static)/+layout.svelte
+++ src/routes/(static)/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <LandingHeader />
-<main class="container flex px-10 md:px-40 mx-auto min-h-[600px]">
+<main class="container flex px-10 md:px-40 mx-auto min-h-[600px] pb-20">
 	<slot />
 </main>
 <Footer />
