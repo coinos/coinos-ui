2f214c30015fd3765f62aa116a5735283503e100
Adam Soltys <asoltys@gmail.com>
2023-02-08 10:14:08 -0800

    comment broken code

--- src/routes/(app)/[username]/(profile)/+page.svelte
+++ src/routes/(app)/[username]/(profile)/+page.svelte
@@ -13,28 +13,28 @@
 </div>
 
 <div>
-	{#each invoice}
-		<a href={`/${user.username}/receive/${id}`}>
-			<div class="border-b p-2 last:border-b-0 hover:bg-gray-100">
-				<div class="flex">
-					<div>
-						<div class="flex">
-							<Avatar user={r} size={20} />
-							<div class="my-auto text-left">
-								<p class="ml-1 text-lg break-words">{r.username}</p>
-								<p class="ml-1 text-secondary">{memo}</p>
-							</div>
-						</div>
-					</div>
-					<div class="whitespace-nowrap my-auto ml-auto flex gap-2">
-						<button class="rounded-full border py-2 px-4 font-bold hover:opacity-80 w-full"
-							>Invoice</button
-						>
-					</div>
-				</div>
-			</div>
-		</a>
-	{/each}
+	<!-- {#each invoice} -->
+	<!-- 	<a href={`/${user.username}/receive/${id}`}> -->
+	<!-- 		<div class="border-b p-2 last:border-b-0 hover:bg-gray-100"> -->
+	<!-- 			<div class="flex"> -->
+	<!-- 				<div> -->
+	<!-- 					<div class="flex"> -->
+	<!-- 						<Avatar user={r} size={20} /> -->
+	<!-- 						<div class="my-auto text-left"> -->
+	<!-- 							<p class="ml-1 text-lg break-words">{r.username}</p> -->
+	<!-- 							<p class="ml-1 text-secondary">{memo}</p> -->
+	<!-- 						</div> -->
+	<!-- 					</div> -->
+	<!-- 				</div> -->
+	<!-- 				<div class="whitespace-nowrap my-auto ml-auto flex gap-2"> -->
+	<!-- 					<button class="rounded-full border py-2 px-4 font-bold hover:opacity-80 w-full" -->
+	<!-- 						>Invoice</button -->
+	<!-- 					> -->
+	<!-- 				</div> -->
+	<!-- 			</div> -->
+	<!-- 		</div> -->
+	<!-- 	</a> -->
+	<!-- {/each} -->
 
 	{#each received as { id, invoice, memo, requester: r }}
 		<a href={`/${user.username}/receive/${id}`}>
