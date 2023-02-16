c20c7c6f7dcc63f3aaddc0fcb5880da0982acaef
Adam Soltys <asoltys@gmail.com>
2023-02-15 22:47:27 -0800

    buy page

--- src/routes/(app)/buy/+page.server.js
+++ src/routes/(app)/buy/+page.server.js
@@ -1,5 +1,5 @@
 import { env } from '$env/dynamic/private';
-import { error, redirect } from '@sveltejs/kit';
+import { error, fail, redirect } from '@sveltejs/kit';
 import { post, auth } from '$lib/utils';
 import { createTicket } from '$lib/ticket';
 
@@ -7,13 +7,21 @@ export const actions = {
 	default: async ({ cookies, request }) => {
 		let form = await request.formData();
 
-		await post('/buy', {
-			number: form.get('number').replace(/\s+/g, ''),
-			month: form.get('expiry').slice(0, 2),
-			year: form.get('expiry').slice(-2),
-			cvc: form.get('cvc')
-		}, auth(cookies));
+		let amount = parseFloat(form.get('amount'));
+		if (!amount || amount < 0 || amount > 500) return fail(400, { message: 'Invalid amount' });
 
-		throw redirect(307, `/launch/thanks/${asset}`);
+		await post(
+			'/buy',
+			{
+        amount: form.get('amount'),
+				number: form.get('number').replace(/\s+/g, ''),
+				month: form.get('expiry').slice(0, 2),
+				year: form.get('expiry').slice(-2),
+				cvc: form.get('cvc')
+			},
+			auth(cookies)
+		);
+
+		throw redirect(307, `/${form.get('username')}/payments`);
 	}
 };
