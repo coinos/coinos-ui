cbf0f0d414a7b8c5ac2a16b12eed575b533991ac
Adam Soltys <asoltys@gmail.com>
2023-03-01 22:54:28 -0800

    disable prerendering

--- src/routes/(app)/support/+server.js
+++ src/routes/(app)/support/+server.js
@@ -1,6 +1,8 @@
 import { json } from '@sveltejs/kit';
 import { auth, post } from '$lib/utils';
 
+export const prerender = false;
+
 export async function POST({ cookies, request }) {
 	let result = await post('/email', await request.json(), auth(cookies));
 	return json(result);
