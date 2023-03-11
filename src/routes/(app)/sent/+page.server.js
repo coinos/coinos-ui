3333b579081d1f3cae8cf0a6ba685d1e00852539
Adam Soltys <asoltys@gmail.com>
2023-03-11 10:21:02 -0800

    prompt for tip on internal payments

--- src/routes/(app)/sent/+page.server.js
+++ src/routes/(app)/sent/+page.server.js
@@ -2,5 +2,6 @@ import { get, auth } from '$lib/utils';
 
 export let load = async ({ cookies }) => {
 	let { payments } = await get('/payments', auth(cookies));
+  console.log("HUH", payments[0])
 	return { payments };
 };
