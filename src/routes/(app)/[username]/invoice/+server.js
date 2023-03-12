93b422023e8190908027b5ea96755d465074e3a0
Adam Soltys <asoltys@gmail.com>
2023-03-11 17:47:15 -0800

    fix param name

--- src/routes/(app)/[username]/invoice/+server.js
+++ src/routes/(app)/[username]/invoice/+server.js
@@ -1,5 +1,7 @@
 import { auth, post } from '$lib/utils';
 import { json } from '@sveltejs/kit';
 
-export let POST = async ({ cookies: c, request: r }) =>
-	json(await post('/invoice', await r.json(), auth(c)));
+export let POST = async ({ cookies: c, request: r }) => {
+	let j = await r.json();
+	return json(await post('/invoice', j, auth(c)));
+};
--- src/routes/(app)/send/invoice/[id]/+page.server.js
+++ src/routes/(app)/send/invoice/[id]/+page.server.js
@@ -4,18 +4,27 @@ import { fd, auth, get, post } from '$lib/utils';
 export async function load({ params: { id }, parent, url }) {
 	let { user } = await parent();
 
-	let { amount, address, rate, prompt, tip, text, user: recipient } = await get(`/invoice/${id}`);
+	let {
+		amount,
+		address,
+		hash,
+		rate,
+		prompt,
+		tip,
+		text,
+		user: recipient
+	} = await get(`/invoice/${id}`);
 
 	if (prompt && tip === null) throw redirect(307, `/${recipient.username}/invoice/${id}/tip`);
 	if (recipient.username === user.username) throw error(500, { message: 'Cannot send to self' });
-	return { amount, address, tip, rate, payreq: text, recipient };
+	return { amount, address, hash, tip, rate, payreq: text, recipient };
 }
 
 export const actions = {
-	default: async ({ cookies, params: { hash }, request }) => {
+	default: async ({ cookies, params: { id }, request }) => {
 		try {
 			let body = await fd(request);
-			body.hash = hash;
+			body.hash = id;
 
 			await post('/payments', body, auth(cookies));
 		} catch (e) {
