fe520d01aff30c8abb09cc2bad5ed46bda3a0889
Adam Soltys <asoltys@gmail.com>
2023-03-21 00:19:29 -0700

    send username with support form

--- src/routes/(app)/admin/+page.server.js
+++ src/routes/(app)/admin/+page.server.js
@@ -1,10 +1,10 @@
-import { get } from '$lib/utils';
+import { auth, get } from '$lib/utils';
 import { redirect } from '@sveltejs/kit';
 
-export async function load({ parent }) {
+export async function load({ cookies, parent }) {
 	let { user } = await parent();
 	if (!user) throw redirect(307, '/login');
 
-	let users = (await get('/users')).sort((a, b) => b.balance - a.balance);
+	let users = (await get('/users', auth(cookies))).sort((a, b) => b.balance - a.balance);
 	return { users };
 }
