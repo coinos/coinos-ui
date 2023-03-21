f6de1343310ffca90bed4ffe3cf2253b310ddf12
Adam Soltys <asoltys@gmail.com>
2023-03-21 00:25:28 -0700

    unload captcha

--- src/routes/(app)/support/+page.svelte
+++ src/routes/(app)/support/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { onDestroy } from 'svelte';
 	import { fail, success, post } from '$lib/utils';
 	import { scale } from 'svelte/transition';
 	import { AppHeader, Icon } from '$comp';
@@ -14,12 +15,13 @@
 	let email;
 	let message;
 	let sent;
+  let recaptchaSiteKey = '6LfCd8YkAAAAANmVJgzN3SQY3n3fv1RhiS5PgMYM';
 
 	let submit = (e) => {
 		e.preventDefault();
 		grecaptcha.ready(() => {
 			grecaptcha
-				.execute('6LfCd8YkAAAAANmVJgzN3SQY3n3fv1RhiS5PgMYM', { action: 'submit' })
+				.execute(recaptchaSiteKey, { action: 'submit' })
 				.then((token) =>
 					post('/support', { username: user?.username, email, message, token })
 						.then(() => (sent = true))
@@ -27,6 +29,20 @@
 				);
 		});
 	};
+
+	onDestroy(() => {
+			const nodeBadge = document.querySelector('.grecaptcha-badge');
+			if (nodeBadge) {
+				document.body.removeChild(nodeBadge.parentNode);
+			}
+
+			const scriptSelector =
+				"script[src='https://www.google.com/recaptcha/api.js?render=" + recaptchaSiteKey + "']";
+			const script = document.querySelector(scriptSelector);
+			if (script) {
+				script.remove();
+			}
+	});
 </script>
 
 <svelte:head>
