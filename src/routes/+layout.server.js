cfb288a96b662ce6e69ed9a0a505e0a46b395530
Adam Soltys <asoltys@gmail.com>
2023-04-29 23:44:29 -0700

    get user locale from db

---
+++ src/routes/+layout.server.js
@@ -0,0 +1,17 @@
+import { auth, get } from "$lib/utils";
+import { loadTranslations, locale } from '$lib/translations';
+export const load = async ({ cookies, url }) => {
+	let user;
+	let token = cookies.get('token');
+	if (token) {
+		try {
+			user = await get('/me', auth(cookies));
+		} catch (e) {}
+	}
+
+	const { pathname } = url;
+	const defaultLocale = user?.language || 'en'; // get from cookie, user session, ...
+	const initLocale = locale.get() || defaultLocale; // set default if no locale already set
+	await loadTranslations(initLocale, pathname); // keep this just before the `return`
+	return {};
+};
