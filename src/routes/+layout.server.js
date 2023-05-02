7f3dc05c74586f4777c4a51ebf2c831da91c6c0c
Adam Soltys <asoltys@gmail.com>
2023-05-02 14:09:51 -0700

    nfc wip

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,8 +1,8 @@
 import { loadTranslations, locale } from '$lib/translations';
 export const load = async ({ url }) => {
-	const { pathname } = url;
+	const { host, pathname } = url;
 	const defaultLocale = 'en'; // get from cookie, user session, ...
 	const initLocale = locale.get() || defaultLocale; // set default if no locale already set
 	await loadTranslations(initLocale, pathname); // keep this just before the `return`
-	return {};
+	return { host, pathname };
 };
