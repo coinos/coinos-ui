508e68d1206ae99650a6c54d43b80df6f06594a4
Adam Soltys <asoltys@gmail.com>
2023-05-21 20:53:39 -0700

    uncomment locations code

--- src/routes/(app)/locations/+server.js
+++ src/routes/(app)/locations/+server.js
@@ -4,12 +4,12 @@ import { json } from '@sveltejs/kit';
 export async function GET({ fetch, setHeaders }) {
 	let locations = [];
 
-	// try {
-	// 	({ locations } = await get('/locations'));
-	// 	setHeaders({ 'cache-control': 'public, max-age=600' });
-	// } catch (e) {
-	// 	console.log('failed to fetch locations');
-	// }
+	try {
+		({ locations } = await get('/locations'));
+		setHeaders({ 'cache-control': 'public, max-age=600' });
+	} catch (e) {
+		console.log('failed to fetch locations');
+	}
 
 	return json({ locations });
 }
