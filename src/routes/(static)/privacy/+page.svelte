a6de09efe319b5fe942b55787f4cfb26d31caf07
Adam Soltys <asoltys@gmail.com>
2023-10-17 12:24:51 -0700

    link to term

--- src/routes/(static)/privacy/+page.svelte
+++ src/routes/(static)/privacy/+page.svelte
@@ -1,20 +1,67 @@
-<h1>Privacy Policy</h1>
-<p>
-At Swapee.me, creators of the mystical Lightning Wallet, we hold your privacy and personal information in the highest regard. This unique privacy policy outlines our commitment to safeguarding your data and ensuring a secure and enchanting experience while using our extraordinary Service.
-</p>
-<h2>
-Collection and Usage of Magical Data
-</h2>
-<p>
-To provide you with an unparalleled experience, we may request certain personally identifiable information. Rest assured that we will handle this information with the utmost care, treating it like precious treasure. We may use this data to create a personalized and magical journey for you, enhancing the powers of our Service. We solemnly swear never to use your information for evil or share it with malevolent beings.
-</p>
-<h2>
-Mysterious Third-Party Partners
-</h2>
-<p>
-In our quest to enhance the enchantment of our Service, we may collaborate with mystical third-party services. These sorcerers of technology might gather information about your mystical journey with us. However, fear not, as their intentions are honorable, and they are bound by magical contracts not to use your information for nefarious purposes. We encourage you to explore the secret scrolls containing the privacy policies of these mystical allies:
-</p>
-<ul>
-  <li>Google Play Services</li>
-  <li>Firebase Analytics</li>
-  </ul>
+<div class="block space-y-5">
+	<h1 class="text-2xl">Privacy Policy</h1>
+	<p>
+		At Swapee.me, creators of the mystical Lightning Wallet, we hold your privacy and personal
+		information in the highest regard. This unique privacy policy outlines our commitment to
+		safeguarding your data and ensuring a secure and enchanting experience while using our
+		extraordinary Service.
+	</p>
+	<h2 class="text-xl">Collection and Usage of Magical Data</h2>
+	<p>
+		To provide you with an unparalleled experience, we may request certain personally identifiable
+		information. Rest assured that we will handle this information with the utmost care, treating it
+		like precious treasure. We may use this data to create a personalized and magical journey for
+		you, enhancing the powers of our Service. We solemnly swear never to use your information for
+		evil or share it with malevolent beings.
+	</p>
+	<h2 class="text-xl">Mysterious Third-Party Partners</h2>
+	<p>
+		In our quest to enhance the enchantment of our Service, we may collaborate with mystical
+		third-party services. These sorcerers of technology might gather information about your mystical
+		journey with us. However, fear not, as their intentions are honorable, and they are bound by
+		magical contracts not to use your information for nefarious purposes. We encourage you to
+		explore the secret scrolls containing the privacy policies of these mystical allies:
+	</p>
+	<ul class="ml-2">
+		<li>Google Play Services</li>
+		<li>Firebase Analytics</li>
+	</ul>
+	<h2 class="text-xl">Data Collection and Usage</h2>
+	<p>
+		To offer you an unparalleled experience, we may request certain personally identifiable
+		information, such as your favorite color, the name of your childhood pet, or your preferred
+		pizza topping. Rest assured that we will handle this information with the utmost care, treating
+		it like precious treasure. We may use this data to provide you with a personalized experience
+		and to enhance the mystical powers of our Service. We solemnly swear never to use your
+		information for evil or share it with malevolent wizards.
+	</p>
+	<h2 class="text-xl">Cookies: The Magic Crumbs</h2>
+
+	<p>
+		We do not rely on ordinary cookies; we prefer magical crumbs to enhance our spellcasting
+		abilities. These enchanted morsels contain mystical data and are transmitted to your mystical
+		browser when you visit our website. They reside within the secret recesses of your device,
+		imparting our Service with unique powers. You have the choice to accept or reject these magical
+		crumbs, but be aware that refusing them might diminish the full extent of our enchantment.
+	</p>
+
+	<h2 class="text-xl">The Protection of Secrets</h2>
+	<p>
+		We hold your personal information in the highest regard and take extraordinary measures to
+		protect it. We employ magical safeguards, incantations, and shields woven with the finest
+		cryptographic spells to ensure its safety. While we strive to achieve the highest level of
+		protection, we must acknowledge that no mystical method is foolproof. Therefore, we cannot
+		guarantee the absolute security of your information, but we promise to guard it with our magical
+		prowess.
+	</p>
+
+	<h2 class="text-xl">Portal to Other Worlds</h2>
+
+	<p>
+		Our enchanted Service may contain gateways to other realms and dimensions. Should you venture
+		through these portals by clicking on third-party links, you will find yourself in uncharted
+		territories. Please be aware that these realms are beyond our control, and their arcane
+		practices and privacy incantations may differ. We recommend you review the privacy policies of
+		these mystifying worlds, for we bear no responsibility for their mystical ways.
+	</p>
+</div>
