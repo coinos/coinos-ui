2daee6140cf45493312165569bc7f2dd492555a9
Adam Soltys <asoltys@gmail.com>
2023-11-17 10:25:21 -0800

    map styles

--- src/components/Popup.svelte
+++ src/components/Popup.svelte
@@ -4,37 +4,32 @@
 
 	let addr = (tags) => {
 		let { 'addr:housenumber': house, 'addr:street': street, 'addr:city': city } = tags;
-		if (house) return `${house} ${street}, ${city}`;
-		if (street) return `${street}, ${city}`;
-		if (city) return city;
+		if (house) return `${house} ${street}`;
 		return '';
 	};
 </script>
 
 {#if tags}
-	<span class="block font-bold">{tags.name}</span>
-
-	<span class="block">{addr(tags)}</span>
-
-	{#if tags.phone}
-		<a href="tel:${tags.phone}" class="block">{tags.phone}</a>
-	{/if}
-
-	{#if tags.website}
-		<a href="${tags.website}" target="_blank" rel="noreferrer" class="block text-blue-400 break-all"
-			>{tags.website}</a
-		>
-	{/if}
-
 	{#if tags.user}
-    <a href={`/${tags.user.username}`}>
-		<div class="flex gap-1">
-			<Avatar user={tags.user} size={16} />
-			<div class="my-auto">
-				{tags.user.username}
-			</div>
-		</div>
-    </a>
-	{/if}
+		<a href={`/${tags.user.username}`}>
+			<div class="space-y-5">
+				<div>
+					<span class="block font-bold">{tags.name}</span>
+					<span class="block">{addr(tags)}</span>
+				</div>
 
+				<div class="flex w-full">
+					<div class="flex gap-1 mx-auto">
+						<Avatar user={tags.user} size={16} />
+						<div class="my-auto">
+							{tags.user.username.toLowerCase()}
+						</div>
+					</div>
+				</div>
+      </div>
+    </a>
+	{:else}
+		<span class="block font-bold">{tags.name}</span>
+		<span class="block">{addr(tags)}</span>
+	{/if}
 {/if}
