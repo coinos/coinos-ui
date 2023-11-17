9f42d4e4e7e7baf3697aef0c43393a5a443c3b39
Adam Soltys <asoltys@gmail.com>
2023-11-17 00:46:59 -0800

    add logo to map page

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,8 +1,18 @@
 <script>
-	import { LocationsMap } from '$comp';
+	import { Icon, LocationsMap } from '$comp';
 	export let data;
 
 	let { locations } = data;
 </script>
 
-<LocationsMap {locations} />
+<div class="pt-4">
+	<div class="w-[243px] mx-auto mb-10">
+		<a href="/">
+			<Icon icon="logo" />
+		</a>
+	</div>
+
+	<div class="flex justify-center items-center">
+		<LocationsMap {locations} />
+	</div>
+</div>
