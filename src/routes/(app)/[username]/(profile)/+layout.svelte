3e804e15706a049c67a71c7a4ddc3fc850829f8f
Adam Soltys <asoltys@gmail.com>
2023-11-17 10:36:22 -0800

    remove button margin

--- src/routes/(app)/[username]/(profile)/+layout.svelte
+++ src/routes/(app)/[username]/(profile)/+layout.svelte
@@ -180,7 +180,7 @@
 			{/if}
 
 			{#if user && user.username !== subject.username && subject.pubkey}
-				<div class="w-full flex mb-4">
+				<div class="w-full flex">
 					<a href={`/${subject.username}/messages`} class="mx-auto">
 						<button class="rounded-full border py-3 px-6 font-bold hover:opacity-80 flex w-60">
 							<div class="mx-auto flex">
