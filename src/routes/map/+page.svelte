20214e1f13bd57b2b76c0393e46a212c3281d45c
Adam Soltys <asoltys@gmail.com>
2023-12-02 00:01:14 -0800

    restore maps

---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Icon from "$comp/Icon.svelte";
+  import LocationsMap from "$comp/LocationsMap.svelte";
+  export let data;
+
+  let { locations } = data;
+</script>
+
+<div class="pt-4">
+  <div class="w-[243px] mx-auto mb-4">
+    <a href="/">
+      <Icon icon="logo" />
+    </a>
+  </div>
+
+  <div class="flex justify-center items-center">
+    <LocationsMap {locations} />
+  </div>
+</div>
