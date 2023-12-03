a7a033c081f5767a4cc1348c7bfd6a0ed3eec131
Adam Soltys <asoltys@gmail.com>
2023-12-03 13:54:51 -0800

    restore pin var

--- src/routes/(app)/login/+page.svelte
+++ src/routes/(app)/login/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import RegisterOrLogin from "$comp/RegisterOrLogin.svelte";
   import { onMount } from "svelte";
+  import { pin } from "$lib/store";
 
   onMount(() => {
     $pin = undefined;
