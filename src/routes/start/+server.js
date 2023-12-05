f0cad01e6bd62192f42601b6eb3a961171fe1309
Adam Soltys <asoltys@gmail.com>
2023-12-05 09:33:41 -0800

    check user cookie

--- src/routes/start/+server.js
+++ src/routes/start/+server.js
@@ -2,11 +2,9 @@ import { auth, get } from "$lib/utils";
 import { redirect } from "@sveltejs/kit";
 
 export async function GET({ cookies }) {
-  let token = cookies.get("token");
-  let user;
-  if (token) {
-    user = await get("/me", auth(cookies));
-    throw redirect(307, `/${user.username}`);
+  let token = cookies.get("user");
+  if (user) {
+    throw redirect(307, `/${user}/receive`);
   }
 
   throw redirect(307, "/");
