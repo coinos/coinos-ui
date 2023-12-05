62a813a07abde15de106446e9eff15009922bd11
Adam Soltys <asoltys@gmail.com>
2023-12-05 09:44:31 -0800

    username cookie

--- src/routes/start/+server.js
+++ src/routes/start/+server.js
@@ -2,9 +2,9 @@ import { auth, get } from "$lib/utils";
 import { redirect } from "@sveltejs/kit";
 
 export async function GET({ cookies }) {
-  let user = cookies.get("user");
-  if (user) {
-    throw redirect(307, `/${user}/receive`);
+  let username = cookies.get("username");
+  if (username) {
+    throw redirect(307, `/${username}/receive`);
   }
 
   throw redirect(307, "/");
