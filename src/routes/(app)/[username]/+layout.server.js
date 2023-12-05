0ee7aac85358e4626a06fd4363bcdd2e71188bd3
Adam Soltys <asoltys@gmail.com>
2023-12-05 09:44:31 -0800

    username cookie

--- src/routes/(app)/[username]/+layout.server.js
+++ src/routes/(app)/[username]/+layout.server.js
@@ -1,11 +1,16 @@
 import { get } from "$lib/utils";
 import { error } from "@sveltejs/kit";
 
-export let load = async ({ depends, params: { username } }) => {
+export let load = async ({ cookies, depends, params: { username } }) => {
   depends("app:user");
 
   try {
     let subject = await get(`/users/${username}`);
+
+    let expires = new Date();
+    expires.setSeconds(expires.getSeconds() + 380 * 24 * 60 * 60);
+    cookies.set("username", username, { path: "/", expires });
+
     return { subject };
   } catch (e) {
     throw error(500, "Unable to retrieve user account data");
