8b826b3fac11d1086e1c1874f2c17216654e23e0
Adam Soltys <asoltys@gmail.com>
2023-12-08 11:48:10 -0800

    latest

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,7 +43,7 @@
 		}
 	});
 
-	let username = 'cambievietnamese';
+	let username = 'anatolias';
 	let password = 'password';
 
 	let src = Qr.drawImg(`https://coinos.io/${username}`, { size: 3200 });
@@ -98,18 +98,18 @@
 	</div>
 	<div class="space-y-8 border-8 border-black p-8 w-[710px] mx-auto">
 		<div class="flex w-full justify-center gap-4">
-			<img src="/images/bitcoin.png" class="w-14" />
-			<h3 class="text-5xl font-medium text-center">Pay with Bitcoin</h3>
+			<img src="/images/bitcoin.png" class="w-20" />
+			<h3 class="text-5xl font-medium text-center my-auto">Pay with Bitcoin</h3>
 		</div>
 		<Q {src} />
 		<div class="text-center text-5xl font-bold">coinos.io/{username}</div>
 		<div>
 			<div class="text-xl pt-2 flex gap-6">
-        Trouble scanning? Some wallets can't handle URLs. Try using your regular Camera app or type into your browser.
+        Trouble scanning? Many wallets can't handle URLs. Try using your regular Camera app or just type into your browser.
 			</div>
 		</div>
-		<div class="flex justify-center text-center">
-      <div class="my-auto">Contact <b>support@coinos.io</b> for assistance<br> Made in Vancouver</div>
+		<div class="flex justify-center text-left">
+      <div class="my-auto">Made in Vancouver <br>Contact <b>support@coinos.io</b> for assistance<br> Get a free account in seconds at coinos.io</div>
 			<img src="/images/logo.png" class="ml-auto my-auto" style="width: 180px" />
 		</div>
 	</div>
