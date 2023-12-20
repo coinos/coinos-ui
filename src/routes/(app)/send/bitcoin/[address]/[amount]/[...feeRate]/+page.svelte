b8bc1fd01e75e96486d59e86216dadf9755f2e54
Adam Soltys <asoltys@gmail.com>
2023-12-20 15:01:46 -0800

    absolute path for form action

--- src/routes/(app)/send/bitcoin/[address]/[amount]/[...feeRate]/+page.svelte
+++ src/routes/(app)/send/bitcoin/[address]/[amount]/[...feeRate]/+page.svelte
@@ -45,7 +45,7 @@
   let goBack = () => goto(`/send/bitcoin/${address}`)
 </script>
 
-<button class="ml-5 md:ml-20 mt-5 md:mt-10 hover:opacity-80" on:click={goBack}>
+<button type="button" class="ml-5 md:ml-20 mt-5 md:mt-10 hover:opacity-80" on:click={goBack}>
   <Icon icon="arrow-left" style="w-10" />
 </button>
 
