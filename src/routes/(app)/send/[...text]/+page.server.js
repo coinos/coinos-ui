0ba0fdd344022556603331b742b38dec251b448c
Adam Soltys <asoltys@gmail.com>
2024-01-03 20:11:05 -0800

    handle lnurls with lightning: prefix

--- src/routes/(app)/send/[...text]/+page.server.js
+++ src/routes/(app)/send/[...text]/+page.server.js
@@ -37,6 +37,7 @@ let parse = async (t, host) => {
   if (t.includes(":")) t = t.split(":")[1];
 
   // lightning
+  if (t.toLowerCase().startsWith("lightning:")) t = t.toLowerCase().replace("lightning:", "");
   if (t.toLowerCase().startsWith("ln")) {
     try {
       ({ id } = await get(`/invoice/${t}`));
