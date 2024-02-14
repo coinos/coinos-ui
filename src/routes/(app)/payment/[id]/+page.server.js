65042a27435822baac6b130171efe275aeedb06d
Adam Soltys <asoltys@gmail.com>
2024-02-14 12:49:23 -0800

    bump fee button

--- src/routes/(app)/payment/[id]/+page.server.js
+++ src/routes/(app)/payment/[id]/+page.server.js
@@ -1,6 +1,7 @@
 import { auth, get } from "$lib/utils";
 
-export async function load({ cookies, params }) {
+export async function load({ cookies, depends, params }) {
+  depends("app:payments");
   let payment = await get(`/payments/${params.id}`, auth(cookies));
   return { payment };
 }
