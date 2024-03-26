8aef38668f30aba2090d7f9f8107e8976407798b
Adam Soltys <asoltys@gmail.com>
2024-03-26 08:36:22 -0700

    remove bootstrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -60,24 +60,6 @@
 	
 </script>
 
-<svelte:head>
-	<link
-		href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-		rel="stylesheet"
-		integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-		crossorigin="anonymous"
-	/>
-	<link href="responsive.css" rel="stylesheet" />
-	<link href="style.css" rel="stylesheet" />
-
-	<!-- Bootstrap JS -->
-	<script
-		src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-		crossorigin="anonymous"
-	></script>
-</svelte:head>
-
 <!-- aleart -->
 <div
 	class="top_alert position-relative alert bg_purple mb-0 alert-dismissible fade show border_radius_0"
