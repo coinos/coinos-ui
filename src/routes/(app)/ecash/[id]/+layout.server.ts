379ac2a5c9c5c63a67bb999b6875ecff33a15b2b
Adam Soltys <asoltys@gmail.com>
2024-09-03 14:35:48 -0700

    ecash version toggle

--- src/routes/(app)/ecash/[id]/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import { get } from "$lib/utils";
-
-export async function load({ params }) {
-  let { id } = params;
-  let { token, status } = await get(`/cash/${id}`);
-  let { spent, total, mint, external } = status;
-  return { id, token, spent, total, mint, external };
-}
