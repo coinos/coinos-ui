72b6a5847243f0d64984543eba66b89ef312e872
Adam Soltys <asoltys@gmail.com>
2024-09-20 15:43:01 -0700

    qr page

--- src/routes/(app)/qr/[text]/+page.svelte
+++ src/routes/(app)/qr/[text]/+page.svelte
@@ -7,21 +7,16 @@
 </script>
 
 <div class="container px-4 max-w-4xl mx-auto space-y-5">
-  <div class="flex justify-center items-center">
-    <div
-      class="md:shadow-xl rounded-3xl md:px-10 pt-5 pb-10 space-y-5 w-full md:mx-5"
-    >
-      <Qr {src} />
+  <Qr {src} />
 
-      <button
-        type="button"
-        class="flex gap-2 text-center break-all rounded-2xl hover:opacity-80 py-5 px-6 w-full mx-auto justify-center border text-xl hover:bg-slate-100"
-        on:click={() => copy(text)}
-      >
-        <div class="my-auto">
-          {text}
-        </div>
-      </button>
-    </div>
-  </div>
+  <button
+     type="button"
+     class="flex gap-2 text-center break-all rounded-2xl hover:opacity-80 py-5 px-6 w-full mx-auto justify-center border text-xl hover:bg-slate-100"
+     on:click={() => copy(text)}
+     >
+     <Icon icon="copy" style="w-8" />
+     <div class="my-auto">
+       {text}
+     </div>
+  </button>
 </div>
