4f137b335de8b010004dae42061195ba0979fc31
Adam Soltys <asoltys@gmail.com>
2024-09-24 20:46:48 -0700

    spacing

--- src/components/Account.svelte
+++ src/components/Account.svelte
@@ -13,12 +13,11 @@
   let setAccount = () => (document.cookie = `aid=${id}; path=/; max-age=86400`);
 </script>
 
-
 <a
   href={`/payments`}
-  class="p-4 space-y-5 border-b block last:border-none"
+  class="px-2 space-y-5 border-b block last:border-none"
   on:click={setAccount}
-  >
+>
   <div class="flex">
     <Balance {balance} {user} {rate} />
     {#if seed}
@@ -32,7 +31,7 @@
 
   <div class="flex justify-center w-full text-xl">
     <a href={`/invoice`} class="contents" on:click={setAccount}>
-      <button class="py-2 px-4 hover:opacity-80 flex grow bg-white">
+      <button class="py-5 px-4 hover:opacity-80 flex grow">
         <div class="mx-auto flex gap-2">
           <Icon icon="receive" style="w-8" />
           <div class="my-auto">{$t("user.dashboard.receive")}</div>
@@ -41,7 +40,7 @@
     </a>
 
     <a href={`/scan`} class="contents" on:click={setAccount}>
-      <button class="py-2 px-4 hover:opacity-80 flex shrink bg-white">
+      <button class="py-5 px-4 hover:opacity-80 flex shrink">
         <div class="mx-auto flex gap-2">
           <Icon icon="camera" style="w-8" />
         </div>
@@ -49,16 +48,12 @@
     </a>
 
     <a href={`/send`} class="contents grow" on:click={setAccount}>
-      <button
-                      type="button"
-                      class="py-2 px-6 hover:opacity-80 flex bg-white grow"
-                      >
-                      <div class="mx-auto flex gap-1">
-                        <Icon icon="send" style="w-8" />
-                        <div class="my-auto">{$t("user.dashboard.send")}</div>
-                      </div>
+      <button type="button" class="py-5 px-6 hover:opacity-80 flex grow">
+        <div class="mx-auto flex gap-1">
+          <Icon icon="send" style="w-8" />
+          <div class="my-auto">{$t("user.dashboard.send")}</div>
+        </div>
       </button>
     </a>
   </div>
 </a>
-
