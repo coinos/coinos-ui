44a713c050d55c277635ce8020c250cfe5ad3c3e
Adam Soltys <asoltys@gmail.com>
2024-09-27 09:06:23 -0700

    tap links for accounts

--- src/components/Account.svelte
+++ src/components/Account.svelte
@@ -13,15 +13,29 @@
   let setAccount = () => (document.cookie = `aid=${id}; path=/; max-age=86400`);
 </script>
 
-<a href={`/payments`} class="block space-y-2 border-b-8 border-primary py-4 last:border-b-0 border-dashed" on:click={setAccount}>
+<a
+  href={`/payments`}
+  class="block space-y-2 border-b-8 border-primary py-4 last:border-b-0 border-dashed"
+  on:click={setAccount}
+  data-sveltekit-preload-data="tap"
+>
   <div class="flex">
     <Balance {balance} {user} {rate} />
-    <a href={`/payments`} class="contents" on:click={setAccount}>
+    <a
+      href={`/payments`}
+      class="contents"
+      on:click={setAccount}
+      data-sveltekit-preload-data="tap"
+    >
       <button class="ml-auto flex gap-1 mb-auto pb-4 pl-4">
         <Icon icon="clock" style="w-8 my-auto" />
       </button>
     </a>
-    <a href={`/account/${id}`} class="contents">
+    <a
+      href={`/account/${id}`}
+      class="contents"
+      data-sveltekit-preload-data="tap"
+    >
       <button class="flex gap-1 mb-auto pb-4 pl-4">
         <Icon icon="settings" style="w-8 my-auto" />
       </button>
@@ -29,10 +43,13 @@
   </div>
 
   <div class="flex justify-center w-full text-xl gap-2">
-    <a href={`/invoice`} class="contents" on:click={setAccount}>
-      <button
-        class="py-5 px-6 hover:opacity-80 flex grow rounded-2xl"
-      >
+    <a
+      href={`/invoice`}
+      class="contents"
+      on:click={setAccount}
+      data-sveltekit-preload-data="tap"
+    >
+      <button class="py-5 px-6 hover:opacity-80 flex grow rounded-2xl">
         <div class="mx-auto flex gap-2">
           <Icon icon="receive" style="w-8" />
           <div class="my-auto">{$t("user.dashboard.receive")}</div>
@@ -40,7 +57,12 @@
       </button>
     </a>
 
-    <a href={`/send`} class="contents grow" on:click={setAccount}>
+    <a
+      href={`/send`}
+      class="contents grow"
+      on:click={setAccount}
+      data-sveltekit-preload-data="tap"
+    >
       <button
         type="button"
         class="py-5 px-6 hover:opacity-80 flex grow rounded-2xl"
