7e5fb8f65a85faa3e11d42ef5ab205aab502e86b
Adam Soltys <asoltys@gmail.com>
2024-09-28 07:47:07 -0700

    password reset

--- src/routes/(app)/[username]/(profile)/+layout.svelte
+++ src/routes/(app)/[username]/(profile)/+layout.svelte
@@ -63,7 +63,7 @@
 
   let reset = async () => {
     try {
-      await post(`/${stripped}/reset`, { password });
+      await post(`/reset`, { username, password });
       success("Password reset");
     } catch (e) {
       fail(e.message);
