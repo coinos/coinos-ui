15ff83a6593e3893a26f247eaac591a695283258
Adam Soltys <asoltys@gmail.com>
2024-10-10 12:17:51 -0700

    loading placeholders

--- src/routes/(app)/[username]/(profile)/follows/+page.svelte
+++ src/routes/(app)/[username]/(profile)/follows/+page.svelte
@@ -7,11 +7,9 @@
   import { invalidate } from "$app/navigation";
 
   export let data;
-
   let { follows } = data;
   $: refresh(data);
   let refresh = (data) => ({ follows } = data);
-
 </script>
 
 <div class="px-3 md:px-0 w-full md:w-[400px] mx-auto">
@@ -19,31 +17,35 @@
     {$t("user.following")}
   </h1>
 
-  {#if follows.length}
-    {#if browser}
-      <VirtualScroll data={follows} key="pubkey" let:data pageMode={true}>
-        <a href={`/${data.pubkey}`} data-sveltekit-preload-data="tap">
-          <div class="flex py-4 text-lg text-secondary" :key={data.pubkey}>
-            <div class="mb-auto mr-2">
-              <div class="md:hidden">
-                <Avatar size={12} user={data} disabled={true} />
+  {#await follows}
+    <div>Loading</div>
+  {:then follows}
+    {#if follows.length}
+      {#if browser}
+        <VirtualScroll data={follows} key="pubkey" let:data pageMode={true}>
+          <a href={`/${data.pubkey}`} data-sveltekit-preload-data="tap">
+            <div class="flex py-4 text-lg text-secondary" :key={data.pubkey}>
+              <div class="mb-auto mr-2">
+                <div class="md:hidden">
+                  <Avatar size={12} user={data} disabled={true} />
+                </div>
+                <div class="hidden md:block">
+                  <Avatar size={20} user={data} disabled={true} />
+                </div>
               </div>
-              <div class="hidden md:block">
-                <Avatar size={20} user={data} disabled={true} />
-              </div>
-            </div>
 
-            <div class="w-full flex pb-1 text-black my-auto">
-              <div>
-                <div>{data.display_name || ""}</div>
-                <div class="text-secondary text-base">@{data.name}</div>
+              <div class="w-full flex pb-1 text-black my-auto">
+                <div>
+                  <div>{data.display_name || ""}</div>
+                  <div class="text-secondary text-base">@{data.name}</div>
+                </div>
               </div>
             </div>
-          </div>
-        </a>
-      </VirtualScroll>
+          </a>
+        </VirtualScroll>
+      {/if}
+    {:else}
+      <div>No follows</div>
     {/if}
-  {:else}
-    <div>No follows</div>
-  {/if}
+  {/await}
 </div>
