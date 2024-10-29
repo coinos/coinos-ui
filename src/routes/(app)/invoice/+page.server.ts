04723fcee3091473bb5f9dc297ea82bda13b8154
Adam Soltys <adam@coinos.io>
2024-10-29 14:40:25 -0700

    check for user before preloading

--- src/routes/(app)/invoice/+page.server.ts
+++ src/routes/(app)/invoice/+page.server.ts
@@ -1,29 +1,29 @@
+import type { Invoice } from "$lib/types";
 import { auth, post } from "$lib/utils";
 import { error, redirect } from "@sveltejs/kit";
-import type { Invoice } from "$lib/types";
 
-export let load = async ({ cookies, parent }) => {
-  let aid = cookies.get("aid");
-  let { subject, user, rates } = await parent();
-  if (aid === user.id) redirect(307, `/${user.username}/receive`);
+export const load = async ({ cookies, parent }) => {
+	const aid = cookies.get("aid");
+	let { subject, user, rates } = await parent();
+	if (aid === user.id) redirect(307, `/${user.username}/receive`);
 
-  let invoice: Invoice = {
-    aid,
-    rate: rates[user?.currency || subject?.currency],
-  };
+	let invoice: Invoice = {
+		aid,
+		rate: rates[user?.currency || subject?.currency],
+	};
 
-  if (!user) user = subject;
+	if (!user) user = subject;
 
-  try {
-    invoice = await post("/invoice", { invoice, aid, user }, auth(cookies));
-  } catch (e) {
-    console.log(e);
-    error(500, "Failed to generate invoice");
-  }
+	try {
+		invoice = await post("/invoice", { invoice, aid, user }, auth(cookies));
+	} catch (e) {
+		console.log(e);
+		error(500, "Failed to generate invoice");
+	}
 
-  let { id } = invoice;
+	const { id } = invoice;
 
-  if (invoice.memoPrompt && !invoice.memo) {
-    redirect(307, `/invoice/${id}/memo`);
-  } else redirect(307, `/invoice/${id}`);
+	if (invoice.memoPrompt && !invoice.memo) {
+		redirect(307, `/invoice/${id}/memo`);
+	} else redirect(307, `/invoice/${id}`);
 };
--- src/routes/(app)/invoice/+server.ts
+++ src/routes/(app)/invoice/+server.ts
@@ -1,7 +1,7 @@
 import { auth, post } from "$lib/utils";
 import { json } from "@sveltejs/kit";
 
-export const POST = async ({ cookies: c, request: r }) => {
-	const j = await r.json();
-	return json(await post("/invoice", j, auth(c)));
+export const POST = async ({ cookies, request }) => {
+	const body = await request.json();
+	return json(await post("/invoice", body, auth(cookies)));
 };
