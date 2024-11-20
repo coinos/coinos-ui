a140b43597e5ad1f6f83f63eeefd8de80f9401eb
Adam Soltys <adam@coinos.io>
2024-11-20 14:22:43 -0800

    styles

--- src/components/Slider.svelte
+++ src/components/Slider.svelte
@@ -6,12 +6,7 @@
     max?: number;
   }
 
-  let {
-    value = $bindable(),
-    handle,
-    min = 0,
-    max = 100
-  }: Props = $props();
+  let { value = $bindable(), handle, min = 0, max = 100 }: Props = $props();
 </script>
 
 <input
@@ -20,46 +15,8 @@
   {min}
   {max}
   bind:value
-  class="px-0 py-0 my-5"
-  style="--bgPercent: {((value - min) * 100) / (max - min)}%;"
+  class="range"
   oninput={handle}
 />
 
-<style>
-  #slider {
-    -webkit-appearance: none;
-    width: 100%;
-    height: 6px;
-    border-radius: 5px;
-    background: linear-gradient(
-      to right,
-      black 0%,
-      black var(--bgPercent),
-      #f5f7fa var(--bgPercent),
-      #f5f7fa 100%
-    );
-    outline: none;
-  }
 
-  #slider:active {
-    background-color: black;
-  }
-
-  #slider::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    appearance: none;
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    background: black;
-    cursor: pointer;
-  }
-
-  #slider::-moz-range-thumb {
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    background: black;
-    cursor: pointer;
-  }
-</style>
--- src/routes/(app)/account/seed/+page.svelte
+++ src/routes/(app)/account/seed/+page.svelte
@@ -4,7 +4,6 @@
   import { tick, onMount } from "svelte";
   import { copy } from "$lib/utils";
   import { t } from "$lib/translations";
-  import Icon from "$comp/Icon.svelte";
   import { mnemonic } from "$lib/store";
   import { generateMnemonic } from "@scure/bip39";
   import { wordlist } from "@scure/bip39/wordlists/english";
@@ -27,47 +26,31 @@
     {$t("accounts.seedPhrase")}
   </h1>
 
-  <div class="container w-full mx-auto text-lg px-4 max-w-xl space-y-5">
+  <div class="container w-full mx-auto text-lg px-4 max-w-xl space-y-2">
     {#if $mnemonic}
       <Mnemonic mnemonic={$mnemonic} />
     {/if}
 
-    <div>
-      <button
-        type="button"
-        class="flex gap-2 text-center break-all rounded-2xl hover:opacity-80 py-3 px-4 w-full mx-auto justify-center border whitespace-nowrap mb-2"
-        onclick={generate}
-      >
-        <Icon icon="random" style="w-8 my-auto" />
-        <div class="my-auto">{$t("accounts.regenerate")}</div>
-      </button>
+    <button type="button" class="btn" onclick={generate}>
+      <iconify-icon icon="ph:dice-three-bold" width="32" />
+      <div class="my-auto">{$t("accounts.regenerate")}</div>
+    </button>
 
-      <button
-        type="button"
-        class="flex gap-2 text-center break-all rounded-2xl hover:opacity-80 py-3 px-4 w-full mx-auto justify-center border whitespace-nowrap mb-2"
-        onclick={() => copy($mnemonic)}
-      >
-        <Icon icon="copy" style="w-8 my-auto" />
-        <div class="my-auto">
-          {$t("payments.copy")}
-        </div>
-      </button>
-    </div>
+    <button type="button" class="btn" onclick={() => copy($mnemonic)}>
+      <iconify-icon icon="ph:copy-bold" width="32" />
+      <div class="my-auto">
+        {$t("payments.copy")}
+      </div>
+    </button>
 
     <div class="flex gap-2">
       <a href={`/account/savings`} class="contents">
-        <button
-          type="button"
-          class="border w-full py-5 px-6 text-xl rounded-2xl"
-        >
+        <button type="button" class="btn !w-auto grow">
           {$t("accounts.back")}
         </button>
       </a>
       <a href={`/account/pass`} class="contents">
-        <button
-          type="button"
-          class="border w-full py-5 px-6 text-xl rounded-2xl"
-        >
+        <button type="button" class="btn btn-accent !w-auto grow">
           {$t("accounts.next")}
         </button>
       </a>
