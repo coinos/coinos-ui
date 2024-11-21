4aac26d19d639a26f17b2616fd446ac71014582a
Adam Soltys <adam@coinos.io>
2024-11-20 20:06:46 -0800

    payment color

--- src/routes/(app)/payments/[...page]/+page.svelte
+++ src/routes/(app)/payments/[...page]/+page.svelte
@@ -207,6 +207,7 @@
           <div
             class="grid grid-cols-12 border-b border-base-200 h-16 hover:bg-base-200"
             class:border-b-0={i === payments.length - 1}
+            class:text-error={p.amount < 0}
             onclick={() => goto(`/payment/${p.id}`)}
           >
             <div class="whitespace-nowrap my-auto col-span-3">
