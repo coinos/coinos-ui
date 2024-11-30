4fb57a802a6ea62c4716ff0bdcd91194c6559e13
Adam Soltys <adam@coinos.io>
2024-11-29 22:49:50 -0800

    clean up static files

--- src/service-worker.js
+++ src/service-worker.js
@@ -36,7 +36,6 @@ self.addEventListener("fetch", (event) => {
 	async function respond() {
 		const url = new URL(event.request.url);
 		const cache = await caches.open(CACHE);
-		console.log(url.pathname);
 
 		// `build`/`files` can always be served from the cache
 		if (ASSETS.includes(url.pathname)) {
