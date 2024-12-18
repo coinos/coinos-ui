762f5ee31e4938f610551d8d50596a0dd2f53b95
Adam Soltys <adam@coinos.io>
2024-12-18 13:48:39 -0800

    fix url

--- src/routes/(app)/printer/+page.server.ts
+++ src/routes/(app)/printer/+page.server.ts
@@ -1,10 +1,11 @@
+import { PUBLIC_COINOS_URL } from "$env/static/public";
 import { fd } from "$lib/utils";
 import { hex } from "@scure/base";
 
 export const actions = {
 	default: async ({ fetch, request }) => {
 		const body = await fd(request);
-		const res = await fetch("/api/printer", {
+		const res = await fetch(`${PUBLIC_COINOS_URL}/printer`, {
 			method: "POST",
 			body: JSON.stringify(body),
 			headers: {
