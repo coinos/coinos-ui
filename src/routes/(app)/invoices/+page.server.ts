c655595690c6a290a53ffed734a78de3dd731dba
Adam Soltys <adam@coinos.io>
2024-12-26 14:35:09 -0800

    invoices page

--- src/routes/(app)/invoices/+page.server.ts
+++ src/routes/(app)/invoices/+page.server.ts
@@ -2,6 +2,5 @@ import { auth, get } from "$lib/utils";
 
 export async function load({ cookies }) {
 	const invoices = await get("/invoices", auth(cookies));
-  console.log(invoices.length)
 	return { invoices };
 }
