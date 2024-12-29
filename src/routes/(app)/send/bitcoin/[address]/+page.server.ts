edecdf80a982771b708fc7da5fafb494d5119b12
Adam Soltys <adam@coinos.io>
2024-12-29 11:59:47 -0800

    default account

--- src/routes/(app)/send/bitcoin/[address]/+page.server.ts
+++ src/routes/(app)/send/bitcoin/[address]/+page.server.ts
@@ -4,7 +4,7 @@ import { auth, get } from "$lib/utils";
 export async function load({ cookies, parent }) {
 	const { user } = await parent();
 	const rates = await getRates();
-	const aid = cookies.get("aid");
+	const aid = cookies.get("aid") || user.id;
 	const { balance } = await get(`/account/${aid}`, auth(cookies));
 	return { balance, rate: rates[user.currency] };
 }
