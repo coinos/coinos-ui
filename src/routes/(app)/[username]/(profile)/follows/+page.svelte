aef741ef176c5d9cd7d71c069b6c8e86dd55312e
Adam Soltys <adam@coinos.io>
2025-01-14 16:56:13 -0800

    flexible profile fields

--- src/routes/(app)/[username]/(profile)/follows/+page.svelte
+++ src/routes/(app)/[username]/(profile)/follows/+page.svelte
@@ -39,8 +39,8 @@
 
                 <div class="w-full flex pb-1 text-black my-auto">
                   <div>
-                    <div>{data.display || ""}</div>
-                    <div class="text-secondary text-base">@{data.username}</div>
+                    <div>{data.display || data.display_name || ""}</div>
+                    <div class="text-secondary text-base">@{data.username || data.name}</div>
                   </div>
                 </div>
               </div>
