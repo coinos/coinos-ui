c6add8ed135e23c566b1d9a25651482370989b55
Adam Soltys <adam@coinos.io>
2025-01-15 13:18:34 -0800

    redirect

--- src/routes/(app)/sign/+page.server.ts
+++ src/routes/(app)/sign/+page.server.ts
@@ -1,6 +1,10 @@
 import { auth, fd, post, types } from "$lib/utils";
+import { redirect } from "@sveltejs/kit";
 
-export async function load({ cookies }) {
+export async function load({ cookies, parent, url }) {
+	const { user } = await parent();
+	if (!user) redirect(307, `/register?redirect=${url.pathname}`);
+
 	const invoice = {
 		address_type: "legacy",
 		type: types.bitcoin,
