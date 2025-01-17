7f631de58ad19b68e581310f09a6971e223b2e1e
Adam Soltys <adam@coinos.io>
2025-01-17 06:00:17 -0800

    set lud16

--- src/routes/(app)/e/[id]/+page.server.ts
+++ src/routes/(app)/e/[id]/+page.server.ts
@@ -1,8 +1,14 @@
 import { get } from "$lib/utils";
+import { error } from "@sveltejs/kit";
 
 export async function load({ params }) {
-	const event = await get(`/event/${params.id}`);
-	const author = await get(`/users/${event.pubkey}`);
-	const zaps = await get(`/zaps/${params.id}`);
-	return { author, event, zaps };
+	try {
+		const event = await get(`/event/${params.id}`);
+		const author = await get(`/users/${event.pubkey}`);
+		const zaps = await get(`/zaps/${params.id}`);
+		return { author, event, zaps };
+	} catch (e) {
+		console.log(e);
+		error(500, { message: "Failed to get event" });
+	}
 }
