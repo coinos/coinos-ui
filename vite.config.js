23fc55bc2378d54121d3836d7f55e7756f7b87ed
Adam Soltys <adam@coinos.io>
2025-01-24 15:20:27 -0800

    allow tor hosts

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,11 @@ const config = {
 		},
 	},
 	preview: {
-		allowedHosts: ["coinos.io"],
+		allowedHosts: [
+			"coinos.io",
+			"6un2jrlcumn5vduhbq6i3i2wrwiydamwjwop472obg6cy2bfo6ua3oad.onion",
+			"vm7h454g5hiy2nt3u5o7evtz3vigtypo2mkyausakvdkbmd7wpyhx5qd.onion",
+		],
 	},
 	server: {
 		proxy:
