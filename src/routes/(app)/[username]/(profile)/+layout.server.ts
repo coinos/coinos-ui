38bcc3e6d10f203213eb5bc014f35e3736acf7a1
Adam Soltys <adam@coinos.io>
2025-01-26 07:51:47 -0800

    hide nostr stuff

--- src/routes/(app)/[username]/(profile)/+layout.server.ts
+++ src/routes/(app)/[username]/(profile)/+layout.server.ts
@@ -7,12 +7,16 @@ export async function load({ depends, parent }) {
 	const rates = await getRates();
 	const { subject, user } = await parent();
 
-	const { follows, followers } = await get(`/${subject.pubkey}/count`);
-	let followList = new Promise((r) => r([]));
-	if (user)
-		followList = get(`/${user.pubkey}/follows?pubkeysOnly=true`).catch(
-			() => [],
-		);
+	// const { follows, followers } = await get(`/${subject.pubkey}/count`);
+	// let followList = new Promise((r) => r([]));
+	// if (user)
+	// 	followList = get(`/${user.pubkey}/follows?pubkeysOnly=true`).catch(
+	// 		() => [],
+	// 	);
+
+	const followList = new Promise((r) => r([]));
+	const follows = [];
+	const followers = [];
 
 	return { follows, followers, followList, rate: rates[subject.currency] };
 }
