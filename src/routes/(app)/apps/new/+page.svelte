9464372a1e51593619a24d26699c7fedece01a93
Adam Soltys <adam@coinos.io>
2025-02-19 21:07:55 -0800

    send postmessage from apps/new

--- src/routes/(app)/apps/new/+page.svelte
+++ src/routes/(app)/apps/new/+page.svelte
@@ -1,9 +1,28 @@
 <script>
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
+  import { browser } from "$app/environment";
   import { t } from "$lib/translations";
   import AppForm from "$comp/AppForm.svelte";
   import { untrack } from "svelte";
+  import {
+    PUBLIC_COINOS_PUBKEY as walletPubkey,
+    PUBLIC_COINOS_RELAY as relayUrl,
+  } from "$env/static/public";
+
   let { data, form } = $props();
   let { app, rate, user } = $derived(data);
+  let lud16 = $derived(`${user.username}@${$page.url.host}`);
+
+  onMount(() => {
+    if (browser) {
+      if (window.opener) {
+        window.opener.postMessage({ relayUrl, lud16, walletPubkey }, "*");
+      } else {
+        console.warn("No window.opener found.");
+      }
+    }
+  });
 </script>
 
 <div class="container px-4 max-w-lg mx-auto space-y-5 mt-20">
