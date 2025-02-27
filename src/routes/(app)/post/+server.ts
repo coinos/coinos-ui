fa59d5f7ebd687dfb3e90e2b8197c751cfea2ace
Adam Soltys <adam@coinos.io>
2025-02-26 17:17:28 -0800

    pins

--- src/routes/(app)/post/+server.ts
+++
@@ -1,9 +0,0 @@
-export async function POST({ cookies, params, request }) {
-	const body = await request.json();
-	try {
-		const result = await post("/event", body, auth(cookies));
-		return json(result);
-	} catch (e: any) {
-		error(500, e.message);
-	}
-}
