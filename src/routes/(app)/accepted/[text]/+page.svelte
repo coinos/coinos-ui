366aafb4a0ebc6413cda4453b10432a414c39738
Adam Soltys <adam@coinos.io>
2025-06-24 11:25:16 -0700

    padding

--- src/routes/(app)/accepted/[text]/+page.svelte
+++ src/routes/(app)/accepted/[text]/+page.svelte
@@ -8,21 +8,8 @@
   let url = `https://coinos.io/p/${username}`;
 </script>
 
-<!-- <div class="container px-4 max-w-4xl mx-auto space-y-5"> -->
-<!--   <Qr {text} /> -->
-<!--  -->
-<!--   <button -->
-<!--     type="button" -->
-<!--     class="btn break-all !h-auto flex-nowrap p-4 font-normal leading-normal" -->
-<!--     onclick={() => copy(text)} -->
-<!--   > -->
-<!--     <iconify-icon noobserver icon="ph:copy-bold" width="32"></iconify-icon> -->
-<!--     <div class="my-auto">{text}</div> -->
-<!--   </button> -->
-<!-- </div> -->
-<!--  -->
-
-<div class="border-8 border-black p-2 max-w-[710px] mx-auto mt-20 space-y-5 mx-2">
+<div class="px-2">
+<div class="border-8 border-black p-2 max-w-[710px] mx-auto mt-20 space-y-5">
   <div class="flex gap-2 mx-auto justify-center">
     <img src="/images/bitcoin.png" class="w-16 h-16 my-auto" />
     <h3 class="text-4xl font-bold text-center my-auto">Bitcoin Accepted</h3>
@@ -37,4 +24,4 @@
        />
   </div>
 </div>
-
+</div>
