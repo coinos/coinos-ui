597b2b14c3c9d58f8eb4fad18082d029c1b65b21
Adam Soltys <adam@coinos.io>
2025-08-06 22:59:28 -0700

    handle no invoice

--- src/routes/(app)/ln/[lnurl]/+page.server.ts
+++ src/routes/(app)/ln/[lnurl]/+page.server.ts
@@ -33,19 +33,16 @@ export async function load({ params, parent }) {
 
 		const { pr } = await fetch(url).then((r) => (r as Response).json());
 
-    console.log("PR", pr);
-		const invoice = await get(`/invoice/${pr}`);
-    console.log("INV", invoice);
-		if (invoice) redirect(307, `/invoice/${invoice.id}`);
+		try {
+			const invoice = await get(`/invoice/${pr}`);
+			if (invoice) redirect(307, `/invoice/${invoice.id}`);
+		} catch (e) {}
 
 		let path = `/send/lightning/${pr}`;
 		if (comment) path += `/${encodeURIComponent(comment)}`;
-    console.log("PATH", path)
 		redirect(307, path);
 	} else if (callback.includes(PUBLIC_DOMAIN)) {
-    console.log("CALLBACK", callback);
 		const username = url.split(`https://${PUBLIC_DOMAIN}/p/`)[1];
-    console.log("UN", username);
 		redirect(307, `/pay/${username}`);
 	}
 
