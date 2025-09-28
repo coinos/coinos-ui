51401d6bad6fa73500ea8258812ad705b750c93a
Adam Soltys <adam@coinos.io>
2025-09-28 13:49:03 -0700

    captcha

--- src/routes/(static)/login/+page.server.ts
+++ src/routes/(static)/login/+page.server.ts
@@ -1,20 +1,32 @@
+import { randomUUID } from "crypto";
 import { PUBLIC_COINOS_URL } from "$env/static/public";
 import { fd, get, login } from "$lib/utils";
 import { fail, redirect } from "@sveltejs/kit";
 
+const captchas = new Set();
 export const load = async ({ parent }) => {
 	const { user } = await parent();
 	if (user?.pubkey) redirect(307, `/${user.username}`);
 	const { challenge } = await get("/challenge");
-	return { challenge };
+	const captcha = randomUUID();
+	captchas.add(captcha);
+	setTimeout(() => captchas.delete(captcha), 120000);
+	return { challenge, captcha };
 };
 
 export const actions = {
 	login: async ({ cookies, request }) => {
-    console.log(request.headers.get('referer'));
 		const form = await fd(request);
-		let { username, password, token, loginRedirect } = form;
-		const user = { username, password, token };
+		let { username, password, token, loginRedirect, captcha } = form;
+		console.log(captchas, captchas.has(captcha), captcha);
+		if (!captchas.has(captcha))
+			return fail(400, { error: "Login form expired", message: "Login form expired", ...form });
+
+		const user = {
+			username,
+			password,
+			token,
+		};
 
 		if (loginRedirect === "undefined") loginRedirect = undefined;
 
