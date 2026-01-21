cec9caf777e882dce9ced7945c7d5b97bb1fdcdf
Adam Soltys <adam@coinos.io>
2026-01-21 15:24:25 -0800

    update forgot page

--- src/routes/(static)/forgot/+page.svelte
+++ src/routes/(static)/forgot/+page.svelte
@@ -6,8 +6,6 @@
   import Spinner from "$comp/Spinner.svelte";
   import { fly } from "svelte/transition";
 
-  let { form } = $props();
-  let email = $state();
   let loading;
 </script>
 
@@ -16,51 +14,8 @@
 >
   <h1 class="text-2xl font-bold text-center">{$t("login.reset")}</h1>
 
-  {#if form?.error}
-    <div class="text-red-600 text-center" in:fly>
-      {form.error}
-    </div>
-  {/if}
-
-  {#if form?.success}
-    <div class="text-secondary">
-      {$t("login.resetSent")}
-    </div>
-
-    <div class="flex w-full">
-      <a href="/login" class="btn">
-        {$t("login.continue")}
-      </a>
-    </div>
-  {:else}
-    <form class="space-y-5" method="POST" use:enhance>
-      <div>
-        <input
-          placeholder={$t("login.email")}
-          name="email"
-          type="text"
-          required
-          class="bg-primary"
-          bind:value={email}
-          use:focus
-          autocapitalize="none"
-        />
-      </div>
-
-      <div class="text-secondary">
-        {$t("login.resetDesc")}
-        <a href="mailto:support@coinos.io" class="font-bold"
-          >support@coinos.io</a
-        >
-      </div>
-
-      <button type="submit" class="btn" disabled={loading}>
-        {#if loading}
-          <Spinner />
-        {:else}
-          {$t("login.submit")}
-        {/if}
-      </button>
-    </form>
-  {/if}
+  <div class="text-secondary">
+    {$t("login.resetDesc")}
+    <a href="mailto:support@coinos.io" class="font-bold">support@coinos.io</a>
+  </div>
 </div>
