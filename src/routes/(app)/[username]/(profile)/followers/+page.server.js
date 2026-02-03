55bd2a9db52bf42f9945fa7a54000332b2a15970
Adam Soltys <adam@coinos.io>
2026-02-03 11:46:21 -0800

    remove follows

--- src/routes/(app)/[username]/(profile)/followers/+page.server.js
+++
@@ -1,8 +0,0 @@
-import { get } from "$lib/utils";
-
-export async function load({ depends, parent }) {
-	depends("app:user");
-	const { subject } = await parent();
-	const followers = await get(`/${subject.pubkey}/followers`);
-	return { followers };
-}
--- src/routes/(app)/[username]/(profile)/follows/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { get } from "$lib/utils";
-
-export async function load({ depends, parent }) {
-	depends("app:user");
-	const { subject } = await parent();
-	const follows = await get(`/${subject.pubkey}/follows`);
-
-	return { follows };
-}
