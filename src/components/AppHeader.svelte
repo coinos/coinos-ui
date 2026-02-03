44f09f89eb8f69dbb9bba76e0a55125093e7ecc0
Adam Soltys <adam@coinos.io>
2026-02-03 11:50:33 -0800

    fix menu

--- src/components/AppHeader.svelte
+++ src/components/AppHeader.svelte
@@ -95,7 +95,7 @@
   @reference "../app.css";
 
   .btn-menu {
-    @apply flex justify-center items-center bg-base-100 p-2 rounded-full w-12 h-12 drop-shadow-xl;
+    @apply flex justify-center items-center bg-base-100 p-2 rounded-full w-12 h-12 sm:w-16 sm:h-16 drop-shadow-xl;
   }
 
   header {
