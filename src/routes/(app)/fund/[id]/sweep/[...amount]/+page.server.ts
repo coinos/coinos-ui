8aa3e42c28698fe9816d5925cc9d935cc1c686a8
Adam Soltys <adam@coinos.io>
2026-02-06 22:52:09 -0800

    skip captcha for funds

--- src/routes/(app)/fund/[id]/sweep/[...amount]/+page.server.ts
+++ src/routes/(app)/fund/[id]/sweep/[...amount]/+page.server.ts
@@ -1,9 +1,10 @@
 import { randomName, randomPassword } from "$lib/random";
 import getRates from "$lib/rates";
-import { auth, get, login, post, sats } from "$lib/utils";
+import { auth, get, post, register, sats } from "$lib/utils";
 import { error, redirect } from "@sveltejs/kit";
+import { env } from "$env/dynamic/private";
 
-export async function load({ cookies, params, parent }) {
+export async function load({ cookies, request, params, parent }) {
 	let { user } = await parent();
 	const { id } = params;
 	let [amount, currency] = params.amount.split("/");
@@ -21,59 +22,35 @@ export async function load({ cookies, params, parent }) {
 		amount = Math.round((amount * sats) / rate);
 	}
 
+	const username = randomName() + Math.floor(Math.random() * 99) + 1;
+	const password = randomPassword();
+
 	if (!amount) redirect(307, `/fund/${id}`);
+	if (!user) {
+		const ip = request.headers.get("cf-connecting-ip");
 
-	if (user) {
-		try {
-			({ amount } = await post("/take", { amount, id }, auth(cookies)));
-		} catch (e) {
-			redirect(307, `/fund/${id}`);
-		}
+		user = {
+			username,
+			password,
+			fresh: true,
+		};
 
-		return { amount, currency, id, rate, rates, password: null };
+		let redirectUrl = `/fund/${id}/sweep`;
+		if (params.amount && !params.currency) redirectUrl += `/${params.amount}`;
+		if (params.currency) redirectUrl += `/${params.amount}/params.currency}`;
+
+		const extraHeaders: Record<string, string> = {};
+		if (env.INTERNAL_API_KEY)
+			extraHeaders["x-api-key"] = env.INTERNAL_API_KEY;
+
+		await register(user, ip, cookies, redirectUrl, undefined, extraHeaders);
 	}
 
-	return { amount, currency, id, rate, rates, needsCaptcha: true };
+	try {
+		({ amount } = await post("/take", { amount, id }, auth(cookies)));
+	} catch (e) {
+		redirect(307, `/fund/${id}`);
+	}
+
+	return { amount, currency, id, rate, rates, password };
 }
-
-export const actions = {
-	default: async ({ cookies, request, params }) => {
-		const { id } = params;
-		const form = await request.formData();
-		const recaptcha = form.get("recaptcha") as string;
-		const amount = Number(form.get("amount"));
-		const ip = request.headers.get("cf-connecting-ip");
-		const host = request.headers.get("host");
-
-		const username = randomName() + Math.floor(Math.random() * 99) + 1;
-		const password = randomPassword();
-
-		const user = { username, password, fresh: true, recaptcha };
-
-		const { sk } = await post(
-			"/register",
-			{ user },
-			{ "cf-connecting-ip": ip },
-		);
-
-		await login(user, cookies, ip, host);
-
-		const expires = new Date();
-		expires.setSeconds(expires.getSeconds() + 21000000);
-		cookies.set("sk", sk, {
-			path: "/",
-			expires,
-			httpOnly: false,
-			sameSite: "lax",
-		});
-
-		let swept;
-		try {
-			swept = await post("/take", { amount, id }, auth(cookies));
-		} catch (e) {
-			redirect(307, `/fund/${id}`);
-		}
-
-		return { amount: swept.amount, password, username };
-	},
-};
