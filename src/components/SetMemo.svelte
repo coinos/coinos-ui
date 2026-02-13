7fcccdeaadd85d5d529dac813fa43c728e4f4acb
Adam Soltys <adam@coinos.io>
2026-02-13 11:55:54 -0800

    ui tweaks

--- src/components/SetMemo.svelte
+++ src/components/SetMemo.svelte
@@ -6,14 +6,14 @@
 
 {#if settingMemo}
   <div class="fixed inset-0 bg-base-100/90 overflow-y-auto h-full w-full z-50">
-    <div class="relative mx-auto p-12 max-w-xl shadow-lg rounded-md bg-base-100 space-y-5">
+    <div class="relative mx-auto mt-24 p-12 max-w-xl shadow-lg rounded-md bg-base-100 space-y-5">
       <form onsubmit={setMemo} class="space-y-5">
-        <input use:focus placeholder={t("invoice.memo")} bind:value={memo} />
+        <input type="text" use:focus placeholder={t("invoice.memo")} bind:value={memo} />
         <div class="w-full flex flex-wrap gap-2">
-          <button bind:this={submit} type="submit" onclick={setMemo} class="btn btn-accent">
+          <button bind:this={submit} type="submit" onclick={setMemo} class="btn btn-accent !w-auto grow">
             <div class="my-auto">{t("payments.ok")}</div>
           </button>
-          <button type="button" class="btn" onclick={toggleMemo} onkeydown={toggleMemo}>
+          <button type="button" class="btn !w-auto grow" onclick={toggleMemo} onkeydown={toggleMemo}>
             <div class="my-auto">{t("payments.cancel")}</div>
           </button>
         </div>
